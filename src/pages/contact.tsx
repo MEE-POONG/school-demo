af2da9dfa8e4d172cffd27b1e7f3aa2941cb524b
wechanon <wechanon2001@gmail.com>
2023-07-14 14:23:07 +0700

    kon

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,16 +1,24 @@
 import RootLayout from "@/components/layout"
 
-export default function Contact(){
-    return(
+export default function Contact() {
+    return (
         <RootLayout>
-        <div className="container mx-24 py-28 text-blue-900 text-4xl">
-            <h1 className="font-fontTH">ติดต่อเรา</h1>
-            <div className=" w-screen h-0.5 bg-black mt-2"></div>
-            <h1>
-                
-            </h1>
+            <div className="container ml-60 py-28 text-blue-900 ">
+                <h1 className="font-fontTH text-4xl">ติดต่อเรา</h1>
+                <div className=" w-2/4 h-0.5 bg-black mt-2"></div>
+                <h1 className=" mt-2 text-2xl ">
+                    วิทยาลัยเทคโนโลยีพนมวันท์
+                </h1>
+                <div className="flex flex-row">
+                    <div>ที่อยู่</div>
+                    
 
-        </div>
+                    <div className=" text-slate-500 ml-3">198 ถ.มิตรภาพ-จอหอ ต.บ้านโพธิ์ อ.เมือง จ.นครราชสีมา 30310</div>
+                </div>
+                
+
+
+            </div>
         </RootLayout>
     )
 }
