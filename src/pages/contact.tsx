2fa9afbcbb8198f1494067aeaa6986f9789a207a
wechanon <wechanon2001@gmail.com>
2023-07-14 11:45:33 +0700

    gun

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,6 +1,6 @@
 export default function Contact(){
     return(
-        <div className=" text-blue-900 text-4xl ml-24 col-span-2">
+        <div className=" text-blue-900 text-4xl ml-24 col-span-2 font-fontTH">
             ติดต่อเรา
             <div className=" w-1/2 h-1 bg-black mt-2"></div>
 
