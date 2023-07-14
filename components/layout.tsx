3137667d9ac25076dacd7180f2d09c887e6a9a8e
ChonracahtPond <137247404+ChonracahtPond@users.noreply.github.com>
2023-07-14 09:52:49 +0700

    pond

---
+++ components/layout.tsx
@@ -0,0 +1,18 @@
+import { Inter } from 'next/font/google'
+import React from "react";
+const inter = Inter({ subsets: ['latin'] })
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+   
+      <main className={inter.className}>{children}
+      </main>
+    
+    </>
+  )
+}
