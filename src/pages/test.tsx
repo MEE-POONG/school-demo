1a7dce2f4f634d80c34a59ae87d04a7fff65aa53
Ploy <117616170+yaminP@users.noreply.github.com>
2023-07-15 14:17:03 +0700

    hover

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -5,57 +5,57 @@ export default function Test() {
     return (
         <RootLayout>
             <div className="container mx-auto text-xl font-fontTH font-bold py-24">
-                <div className="flex flex-row justify-center gap-24 mb-8">
-                    <div className="flex text-emerald-600 gap-5">
+                <div className="flex flex-row justify-center gap-10 mb-8">
+                    <div className="flex items-center text-emerald-600 gap-5 bg-gray-600/5 p-6 rounded-md shadow-md hover:scale-105">
                         <BsFill1CircleFill size={50} />
-                        <div>
+                        <div className="w-72 h-40">
                             <h3>หัวข้อ 1</h3>
-                            <p>รายละเอียดของหัวข้อ 1</p>
+                            <p className="text-[16px]">รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1</p>
                         </div>
                     </div>
-                    <div className="flex text-emerald-600 gap-5">
+                    <div className="flex items-center text-emerald-600 gap-5 bg-gray-600/5 p-6 rounded-md shadow-md">
                         <BsFill1CircleFill size={50} />
-                        <div>
+                        <div className="w-72 h-40">
                             <h3>หัวข้อ 1</h3>
-                            <p>รายละเอียดของหัวข้อ 1</p>
-                        </div>
-                    </div>
-                    
-                </div>
-
-                <div className="flex flex-row justify-center gap-24 mb-10">
-                    <div className="flex text-emerald-600 gap-5">
-                        <BsFill1CircleFill size={50} />
-                        <div>
-                            <h3>หัวข้อ 1</h3>
-                            <p>รายละเอียดของหัวข้อ 1</p>
-                        </div>
-                    </div>
-                    <div className="flex text-emerald-600 gap-5">
-                        <BsFill1CircleFill size={50} />
-                        <div>
-                            <h3>หัวข้อ 1</h3>
-                            <p>รายละเอียดของหัวข้อ 1</p>
+                            <p className="text-[16px]">รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1</p>
                         </div>
                     </div>
                 </div>
 
-                <div className="flex flex-row justify-center gap-24">
-                    <div className="flex text-emerald-600 gap-5">
+                <div className="flex flex-row justify-center gap-10 mb-8">
+                    <div className="flex items-center text-emerald-600 gap-5 bg-gray-600/5 p-6 rounded-md shadow-md">
                         <BsFill1CircleFill size={50} />
-                        <div>
+                        <div className="w-72 h-40">
                             <h3>หัวข้อ 1</h3>
-                            <p>รายละเอียดของหัวข้อ 1</p>
+                            <p className="text-[16px]">รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1</p>
                         </div>
                     </div>
-                    <div className="flex text-emerald-600 gap-5">
+                    <div className="flex items-center text-emerald-600 gap-5 bg-gray-600/5 p-6 rounded-md shadow-md">
                         <BsFill1CircleFill size={50} />
-                        <div>
+                        <div className="w-72 h-40">
                             <h3>หัวข้อ 1</h3>
-                            <p>รายละเอียดของหัวข้อ 1</p>
+                            <p className="text-[16px]">รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1</p>
                         </div>
                     </div>
                 </div>
+
+                <div className="flex flex-row justify-center gap-10 mb-8">
+                    <div className="flex items-center text-emerald-600 gap-5 bg-gray-600/5 p-6 rounded-md shadow-md">
+                        <BsFill1CircleFill size={50} />
+                        <div className="w-72 h-40">
+                            <h3>หัวข้อ 1</h3>
+                            <p className="text-[16px]">รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1</p>
+                        </div>
+                    </div>
+                    <div className="flex items-center text-emerald-600 gap-5 bg-gray-600/5 p-6 rounded-md shadow-md">
+                        <BsFill1CircleFill size={50} />
+                        <div className="w-72 h-40">
+                            <h3>หัวข้อ 1</h3>
+                            <p className="text-[16px]">รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1รายละเอียดของหัวข้อ 1</p>
+                        </div>
+                    </div>
+                </div>
+
                 
             </div>
         </RootLayout>
