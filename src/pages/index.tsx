c5af8b99acd0cb654337a96a4baac8ffee8cddde
wechanon <wechanon2001@gmail.com>
2023-07-15 16:27:08 +0700

    lol

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,8 @@ import { RiFilePaper2Line } from "react-icons/ri";
 import { GrMoney } from "react-icons/gr";
 import { RiGraduationCapLine } from "react-icons/ri";
 import { FaUserGraduate } from "react-icons/fa";
+import Link from 'next/link'
+import { FaMoneyBill } from "react-icons/fa";
 
 
 
@@ -18,18 +20,38 @@ export default function Home() {
   return (
     <RootLayout>
       <Slider />
-      <div className="grid grid-cols-4 gap-1 mx-60 text-center">
-        <div className='border-2 rounded-full text-center border-blue-900'><RiFilePaper2Line size={60} /></div>
-        <div><GrMoney size={60}/></div>
-        <div><RiGraduationCapLine size={60} /></div>
-        <div><FaUserGraduate size={60} /></div>
-      </div>
-      <div className="grid grid-cols-4 gap-1 mx-60  text-center ">
-        <div className=' mr-14'>สมัครเรียน</div>
-        <div className=' mr-10'>ทุนการศึกษา</div>
-        <div className=' mr-6'>ปรึกษาเพิ่มเติม</div>
-        <div className=' mr-6'>คณะที่เปิดสอน</div>
-      </div>
+      
+      <div className="grid md:grid-flow-col justify-center gap-3 md:gap-10 mt-6">
+                  
+                  <div className="text-center">
+                      <div className="bg-white text-blue-900 p-3 rounded-full hover:bg-blue-900 hover:text-white border-2 border-blue-900">
+                          <Link href=""><RiFilePaper2Line size={80} /></Link>
+                      </div>
+                      <p className='text-blue-900'>สมัครเรียน</p>
+                  </div>
+
+                  <div className="text-center">
+                      <div className="bg-white text-blue-900 p-3 rounded-full hover:bg-blue-900 hover:text-white border-2 border-blue-900">
+                          <Link href=""><FaMoneyBill size={80}/></Link>
+                      </div>
+                      <p className='text-blue-900'>ทุนการศึกษา</p>
+                  </div>
+        
+                  <div className="text-center">
+                      <div className="bg-white text-blue-900 p-3 rounded-full hover:bg-blue-900 hover:text-white border-2 border-blue-900">
+                          <Link href=""><RiGraduationCapLine size={80} /></Link>
+                      </div>
+                      <p className='text-blue-900'>ปรึกษาเพิ่มเติม</p>
+                  </div>
+
+                  <div className="text-center">
+                      <div className="bg-white text-blue-900 p-3 rounded-full hover:bg-blue-900 hover:text-white border-2 border-blue-900">
+                          <Link href=""><FaUserGraduate size={80} /></Link>
+                      </div>
+                      <p className='text-blue-900'>คณะที่เปิดสอน</p>
+                  </div>
+             
+          </div>
     </RootLayout>
   )
 }
