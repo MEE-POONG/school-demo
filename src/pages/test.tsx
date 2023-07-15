1b5c7b70d244f9c144c6e536abbbc37dab813fd0
Ploy <117616170+yaminP@users.noreply.github.com>
2023-07-15 14:07:36 +0700

    test

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,10 +1,62 @@
 import RootLayout from "@/components/layout";
+import { BsFill1CircleFill } from "react-icons/bs";
 
-export default function Test(){
-    return(
+export default function Test() {
+    return (
         <RootLayout>
-            <div className="text-xl font-fontTH font-bold text-emerald-600">
-                Test ทดสอบ
+            <div className="container mx-auto text-xl font-fontTH font-bold py-24">
+                <div className="flex flex-row justify-center gap-24 mb-8">
+                    <div className="flex text-emerald-600 gap-5">
+                        <BsFill1CircleFill size={50} />
+                        <div>
+                            <h3>หัวข้อ 1</h3>
+                            <p>รายละเอียดของหัวข้อ 1</p>
+                        </div>
+                    </div>
+                    <div className="flex text-emerald-600 gap-5">
+                        <BsFill1CircleFill size={50} />
+                        <div>
+                            <h3>หัวข้อ 1</h3>
+                            <p>รายละเอียดของหัวข้อ 1</p>
+                        </div>
+                    </div>
+                    
+                </div>
+
+                <div className="flex flex-row justify-center gap-24 mb-10">
+                    <div className="flex text-emerald-600 gap-5">
+                        <BsFill1CircleFill size={50} />
+                        <div>
+                            <h3>หัวข้อ 1</h3>
+                            <p>รายละเอียดของหัวข้อ 1</p>
+                        </div>
+                    </div>
+                    <div className="flex text-emerald-600 gap-5">
+                        <BsFill1CircleFill size={50} />
+                        <div>
+                            <h3>หัวข้อ 1</h3>
+                            <p>รายละเอียดของหัวข้อ 1</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex flex-row justify-center gap-24">
+                    <div className="flex text-emerald-600 gap-5">
+                        <BsFill1CircleFill size={50} />
+                        <div>
+                            <h3>หัวข้อ 1</h3>
+                            <p>รายละเอียดของหัวข้อ 1</p>
+                        </div>
+                    </div>
+                    <div className="flex text-emerald-600 gap-5">
+                        <BsFill1CircleFill size={50} />
+                        <div>
+                            <h3>หัวข้อ 1</h3>
+                            <p>รายละเอียดของหัวข้อ 1</p>
+                        </div>
+                    </div>
+                </div>
+                
             </div>
         </RootLayout>
     )
