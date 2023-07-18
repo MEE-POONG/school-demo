84cc386fb560296d8e86656ed9150ea4a23e2caf
wechanon <wechanon2001@gmail.com>
2023-07-18 14:34:54 +0700

    tiw

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -17,52 +17,67 @@ import { FaMoneyBill } from "react-icons/fa";
 const inter = Inter({ subsets: ['latin'] })
 
 export default function About() {
-  return (
-    <RootLayout>
-      <Slider />
+    return (
+        <RootLayout>
+            <Slider />
 
-      <div className="grid md:grid-flow-col justify-center gap-3 md:gap-10 mt-6">
+            <div className="grid md:grid-flow-col justify-center gap-3 md:gap-10 mt-6">
 
-        <div className="text-center">
-          <div className="bg-white text-blue-900 p-3 rounded-full hover:bg-blue-900 hover:text-white border-2 border-blue-900">
-            <Link href=""><RiFilePaper2Line size={80} /></Link>
-          </div>
-          <p className='text-blue-900'>สมัครเรียน</p>
-        </div>
+                <div className="text-center">
+                    <div className="bg-white text-blue-900 p-3 rounded-full hover:bg-blue-900 hover:text-white border-2 border-blue-900">
+                        <Link href=""><RiFilePaper2Line size={70} /></Link>
+                    </div>
+                    <p className='text-blue-900'>สมัครเรียน</p>
+                </div>
 
-        <div className="text-center">
-          <div className="bg-white text-blue-900 p-3 rounded-full hover:bg-blue-900 hover:text-white border-2 border-blue-900">
-            <Link href=""><FaMoneyBill size={80} /></Link>
-          </div>
-          <p className='text-blue-900'>ทุนการศึกษา</p>
-        </div>
+                <div className="text-center">
+                    <div className="bg-white text-blue-900 p-3 rounded-full hover:bg-blue-900 hover:text-white border-2 border-blue-900">
+                        <Link href=""><FaMoneyBill size={70} /></Link>
+                    </div>
+                    <p className='text-blue-900'>ทุนการศึกษา</p>
+                </div>
 
-        <div className="text-center">
-          <div className="bg-white text-blue-900 p-3 rounded-full hover:bg-blue-900 hover:text-white border-2 border-blue-900">
-            <Link href=""><RiGraduationCapLine size={80} /></Link>
-          </div>
-          <p className='text-blue-900'>ปรึกษาเพิ่มเติม</p>
-        </div>
+                <div className="text-center">
+                    <div className="bg-white text-blue-900 p-3 rounded-full hover:bg-blue-900 hover:text-white border-2 border-blue-900">
+                        <Link href=""><RiGraduationCapLine size={70} /></Link>
+                    </div>
+                    <p className='text-blue-900'>ปรึกษาเพิ่มเติม</p>
+                </div>
 
-        <div className="text-center">
-          <div className="bg-white text-blue-900 p-3 rounded-full hover:bg-blue-900 hover:text-white border-2 border-blue-900">
-            <Link href=""><FaUserGraduate size={80} /></Link>
-          </div>
-          <p className='text-blue-900'>คณะที่เปิดสอน</p>
-        </div>
+                <div className="text-center">
+                    <div className="bg-white text-blue-900 p-3 rounded-full hover:bg-blue-900 hover:text-white border-2 border-blue-900">
+                        <Link href=""><FaUserGraduate size={70} /></Link>
+                    </div>
+                    <p className='text-blue-900'>คณะที่เปิดสอน</p>
+                </div>
 
 
 
-      </div>
-      <div className='container mx-auto py-5 md:py-24 text-blue-900'>
-        <h1 className="font-fontTH text-2xl md:text-4xl ml-3 md:ml-0">ข่าวประชาสัมพันธ์</h1>
-        <div className=" w-full h-0.5 bg-blue-900 mt-2"></div>
-        <div className="flex justify-between ...">
-          <div className="order-last">อ่านทั้งหมด</div>
-          <div></div>
-        </div>
-        
-      </div>
-    </RootLayout>
-  )
+            </div>
+            <div className='container mx-auto py-5 md:py-24 text-blue-900'>
+                <h1 className="font-fontTH text-2xl md:text-4xl ml-3 md:ml-0">ข่าวประชาสัมพันธ์</h1>
+                <div className="flex justify-between ...">
+                    <div className="order-last"><Link href="about">อ่านทั้งหมด</Link>
+                    </div>
+                    <div></div>
+                </div>
+                <div className=" w-full h-0.5 bg-blue-900 mb-7">
+                </div>
+                <img src="/img/imgSlider/school1.jpg" width="800px" height="100px" />
+                < div className="grid grid-cols-3 gap-3 mt-5">
+                   <img src="/img/imgSlider/school6.jpg" width="250px" height="100px" />
+                    <img src="/img/register/regis.png" width="250px" height="100px"/>
+                    <img src="/img/imgSlider/school3.jpg" width="250px" height="100px" />
+                </div>
+
+
+
+
+
+
+
+
+            </div>
+        </RootLayout>
+    )
 }
