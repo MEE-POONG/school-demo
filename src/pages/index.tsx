380501c3a38885fafaeb3dd6cb0803dac3b60e25
ChonracahtPond <137247404+ChonracahtPond@users.noreply.github.com>
2023-07-21 16:03:25 +0700

    pond

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import RootLayout from "../../components/layout";
 import Footer from "@/components/footer";
-import Navbar from "@/pages/navbar";
+import Navbar from "@/components/navbar";
 import Slider from "@/pages/slider";
 import { RiFilePaper2Line } from "react-icons/ri";
 import { GrMoney } from "react-icons/gr";
