93809b4ccd63b7a9a8c0e4d468617684e5ed2041
ChonracahtPond <137247404+ChonracahtPond@users.noreply.github.com>
2023-07-26 16:35:26 +0700

    pond

--- src/pages/testpond/test2.tsx
+++ src/pages/testpond/test2.tsx
@@ -1,33 +1,119 @@
-import React, { useState } from 'react';
+import Link from "next/link";
+import React from "react";
 
-const Dropdown: React.FC = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleDropdown = () => {
-    setIsOpen((prevIsOpen) => !prevIsOpen);
-  };
-
+const Footer = () => {
   return (
-    <div className="dropdown-container">
-      <button
-        className="dropdown-toggle"
-        onMouseEnter={toggleDropdown}
-        onMouseLeave={toggleDropdown}
-      >
-        Dropdown Toggle
-      </button>
-      {isOpen && (
-        <div className="dropdown-menu">
-          {/* เนื้อหาของ Dropdown Menu */}
-          <ul>
-            <li>เมนูที่ 1</li>
-            <li>เมนูที่ 2</li>
-            <li>เมนูที่ 3</li>
-          </ul>
+    <footer className="bg-[#1F306A] w-full font-fontTH">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-col md:flex-row items-center justify-between">
+          <Link href="/" className="mb-4 flex">
+            <img
+              src="/img/Logo_panomwan.png"
+              className="h-[70px] mr-3"
+              alt="Flowbite Logo"
+            />
+
+            <span className="self-center text-x font-semibold text-gray-300">
+              <p>PHANOMWAN COLLEGE OF TECHNOLOGY</p>
+              <hr className="w-[320px] h-[2px] bg-white" />
+              <p>198 ถ.มิตรภาพ-จอหอ ต.บ้านโพธิ์ อ.เมือง จ.นครราชสีมา 30310</p>
+              <p>TEL : 044-955121-122 FAX : 044-955120</p>
+            </span>
+          </Link>
+
+          <div className="md:flex text-gray-500 my-8">
+            <div className="md:mr-10">
+              <h2 className="mb-6 text-md font-semibold text-white">
+                คณะที่เปิดสอน
+              </h2>
+              <ul className="text-gray-500">
+                <li className="mb-4">
+                  <Link
+                    href="https://flowbite.com/"
+                    className="hover:underline text-white"
+                  >
+                    คณะบริหารธรุกิจ
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link
+                    href="https://flowbite.com/"
+                    className="hover:underline text-white"
+                  >
+                    คณะวิทยาศาสตร์และเทคโนโลยี
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link
+                    href="https://flowbite.com/"
+                    className="hover:underline text-white"
+                  >
+                    คณะวิทยาศาสตร์
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://tailwindcss.com/"
+                    className="hover:underline text-white"
+                  >
+                    คณะศิลปศาสตร์
+                  </Link>
+                </li>
+              </ul>
+            </div>
+
+            <div className="">
+              <h2 className="mb-6 text-sm font-semibold text-white ">
+                รายการ
+              </h2>
+              <ul className="text-white">
+                <li className="mb-4">
+                  <Link
+                    href=""
+                    className="hover:underline "
+                  >
+                    สมัครเรียน
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link
+                    href="https://tailwindcss.com/"
+                    className="hover:underline "
+                  >
+                    ปรึกษาเรื่องทุน
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link
+                    href="https://tailwindcss.com/"
+                    className="hover:underline "
+                  >
+                    ปรึกษาเรื่องสมัครเรียน
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-      )}
-    </div>
+      </div>
+      <div className=" bg-black h-9">
+        {/* <hr className="" /> */}
+        <span className="block text-sm">
+          © 2023{" "}  
+          <Link href="" className="hover:underline ">
+            Me Prompt Technology CONPANY LIMITED.
+          </Link>
+          All Rights Reserved.
+        </span>
+      </div>
+    </footer>
+
+    
   );
 };
 
-export default Dropdown;
+export default Footer;
+
+
+
+
