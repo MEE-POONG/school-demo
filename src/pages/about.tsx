943e609d8211a05b9befe46d2f6cc1a054fb7836
ken1lnw <moowinza@gmail.com>
2023-07-26 15:17:14 +0700

    about page complete

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -17,34 +17,43 @@ export default function About() {
     return (
         <RootLayout>
 
+            {/* banner คณะและหลักสูตร */}
+            <div className="relative">
+            <img src="/img/about/building.png" alt="" />
+                <div className="absolute inset-0  mb-2 md:mb-24  flex justify-center items-center">
+                    {/* <h1 className='text-white text-xl md:text-6xl lg:text-7xl '>
+                    วิทยาลัยเทคโนโลยีพนมวันท์
+                    </h1>    */}
+                </div>
+            </div>
 
-            {/* div container */}
-            <div className="container mx-auto text-xl py-8 md:py-24 ">
-
-                <div className='flex justify-between'>
-                    <div>
-                        <h1 className='text-2xl  md:text-5xl text-[#1F306A] mb-2'>
+            {/* แทบ รู้จักพนมวันท์ */}
+            <div className=''>
+                    <div className="bg-[#1F306A] bg-opacity-80">
+                        <h1 className='text-xl md:text-5xl text-[#FFBF00] text-center py-4 md:py-10'>
                         รู้จัก”พนมวันท์”
                         </h1>
                     
                     </div>
 
-                </div>
-                {/* div ขีด */}
-                <div className='bg-[#1F306A] w-full h-1  mb-7 md:mb-14'></div>
-                {/*  */}
+            </div>
+
+            {/* พื้นหลังใหญ่นอก container */}
+            <div className=" bg-black bg-opacity-80">
+            {/* div container */}
+            <div className="container mx-auto text-xl py-8 md:py-14 px-2 ">
 
                     {/* วิทยาลัยเทคโนโลยีพนมวันท์ และรูป  */}
                     <div>
-                        <h1 className="text-[#1F306A] font-bold text-2xl md:text-4xl">วิทยาลัยเทคโนโลยีพนมวันท์</h1>
+                        <h1 className="text-[#6CB4EE] font-bold text-2xl md:text-4xl">วิทยาลัยเทคโนโลยีพนมวันท์</h1>
                         <img className='py-4' src="/img/school/panomtext.png" alt="" />
                     </div>
 
                     {/* ข้อมูลสถาบัน  */}
 
-                    <h1 className="text-[#1F306A] text-2xl md:text-3xl py-5">ข้อมูลสถาบัน</h1>
+                    <h1 className="text-[#2797FF] text-2xl md:text-3xl py-5">ข้อมูลสถาบัน</h1>
                     
-                    <div>
+                    <div className="text-white">
                         <p className="mb-4">
                             วิทยาลัยเทคโนโลยีพนมวันท์ ก่อตั้งขึ้นเมื่อวันที่ 21 พฤศจิกายน 2550 ด้วยเจตนารมณ์ที่ตระหนักถึงความสำคัญของการศึกษา
                             ในระดับปริญญาตรีอันจะนำไปสู่การพัฒนาทางด้านความคิดทักษะเชิงวิชาชีพมนุษยสัมพันธ์และคุณธรรม
@@ -64,31 +73,31 @@ export default function About() {
                     </div>
 
                     {/* ตราสัญลักษณ์ของวิทยาลัยเทคโนโลยีพนมวันท์  */}
-                    <h1 className="text-[#1F306A] text-xl md:text-3xl py-5">ตราสัญลักษณ์ของวิทยาลัยเทคโนโลยีพนมวันท์</h1>
+                    <h1 className="text-[#2797FF] text-xl md:text-3xl py-5">ตราสัญลักษณ์ของวิทยาลัยเทคโนโลยีพนมวันท์</h1>
                     <div>
                         <img className='w-80 h-80 p-12' src="/img/school/logo panomwan.png" alt="" />
-                        <p className="mb-10">
+                        <p className="mb-10 text-white">
                             สัญลักษณ์ของวิทยาลัยเทคโนโลยีพนมวันท์มีเครื่องหมายเป็นรูปตรา
                             วงกลม ภานในวงกลม เป็นรูปดอกบัวบาน 8  กลีบล้อมรอบ
                             ปราสาทหิน
                         </p>
                         
                         <div className="md:flex">
-                            <div className="text-[#0000B8] mr-2">ดอกบัวบาน 8 กลีบ </div><div>หมายถึง ทางแห่งความสำเร็จมรรค 8 </div>
+                            <div className="text-[#6CB4EE] mr-2">ดอกบัวบาน 8 กลีบ </div><div className="text-white">หมายถึง ทางแห่งความสำเร็จมรรค 8 </div>
                         </div>
                         <div className="md:flex">
-                            <div className="text-[#0000B8] mr-2">กลีบบัวทั้ง 8 </div><div>หมายถึง ปรัชญาและปณิธานของวิทยาลัย </div>
+                            <div className="text-[#6CB4EE] mr-2">กลีบบัวทั้ง 8 </div><div className="text-white">หมายถึง ปรัชญาและปณิธานของวิทยาลัย </div>
                         </div>
                         <div className="md:flex">
-                            <div className="text-[#0000B8] mr-2">ยอดปราสาทหิน </div><div> หมายถึง ความสูงสุด ความแข็งแกร่งอุตสาหะ พากเพียร ศิลปและวัฒนธรรมท้องถิ่น </div>
+                            <div className="text-[#6CB4EE] mr-2">ยอดปราสาทหิน </div><div className="text-white"> หมายถึง ความสูงสุด ความแข็งแกร่งอุตสาหะ พากเพียร ศิลปและวัฒนธรรมท้องถิ่น </div>
                         </div>
                         <div className="md:flex">
-                            <div className="text-[#0000B8] mr-2">ปรัชญาและปณิธานของวิทยาลัย </div><div> ฝีมือ เทคโนโลยี วิจัย วัฒนธรรม คุณธรรม ความก้าวหน้า ความสำเร็จและสนองสังคม </div>
+                            <div className="text-[#6CB4EE] mr-2">ปรัชญาและปณิธานของวิทยาลัย </div><div className="text-white"> ฝีมือ เทคโนโลยี วิจัย วัฒนธรรม คุณธรรม ความก้าวหน้า ความสำเร็จและสนองสังคม </div>
                         </div>
                     </div>
 
                     {/* สีของวิทยาลัยเทคโนโลยีพนมวันท์  */}
-                    <h1 className="text-[#1F306A] text-xl md:text-3xl py-5">สีของวิทยาลัยเทคโนโลยีพนมวันท์</h1>
+                    <h1 className="text-[#2797FF] text-xl md:text-3xl py-5">สีของวิทยาลัยเทคโนโลยีพนมวันท์</h1>
                     <div className="p-10">
                         <div className="bg-[#1F306A] w-60 h-20"></div>
                         <div className="bg-[#FFBF00] w-60 h-20"></div>
@@ -96,13 +105,13 @@ export default function About() {
 
 
                     <div className="md:flex">
-                        <div className="">สีของวิทยาลัยเทคโนโลยีพนมวันท์ คือ น้ำเงินและเหลืองทอง</div>
+                        <div className="text-white">สีของวิทยาลัยเทคโนโลยีพนมวันท์ คือ น้ำเงินและเหลืองทอง</div>
                     </div>
                     <div className="md:flex">
-                        <div className="text-[#0000B8] mr-2">น้ำเงิน</div><div>หมายถึง ความลุ่มลึก ความหนักแน่น มั่นคงในเชิง วิชาการ เปรียบ เสมือนน้ำทะเลสีน้ำเงิน  </div>
+                        <div className="text-[#0000B8] mr-2">น้ำเงิน</div><div className="text-white">หมายถึง ความลุ่มลึก ความหนักแน่น มั่นคงในเชิง วิชาการ เปรียบ เสมือนน้ำทะเลสีน้ำเงิน  </div>
                     </div>
                     <div className="md:flex">
-                        <div className="text-[#FFBF00] mr-2 ">เหลืองทอง </div><div className="mb-20"> หมายถึง ความมีชื่อเสียง คุณธรรม คุณภาพ และเป็นเอกลักษณ์ ของความยิ่งใหญ่ </div>
+                        <div className="text-[#FFBF00] mr-2 ">เหลืองทอง </div><div className="mb-20 text-white"> หมายถึง ความมีชื่อเสียง คุณธรรม คุณภาพ และเป็นเอกลักษณ์ ของความยิ่งใหญ่ </div>
                     </div>
 
 
@@ -114,13 +123,9 @@ export default function About() {
 
 
 {/* ท่อนคณะกรรมการ */}
-                {/* div ขีด */}
-                <div className='bg-[#1F306A] w-full h-1  mb-7 md:mb-7'></div>
-                {/* */}
-
                 {/* แทบ คณะกรรมการสภาวิทยาลัย */}
 
-                <div className="bg-[#1F306A] h-32 justify-center flex flex-col mb-10">
+                <div className="bg-[#2797FF] h-32 justify-center flex flex-col mb-10">
                     <div className="text-white text-2xl  md:text-4xl text-center mb-2">
                     <h1>คณะกรรมการสภาวิทยาลัย</h1>
                     </div>
@@ -133,9 +138,9 @@ export default function About() {
                 {/* นายกสภาวิทยาลัย  */}
                 <div className="columns-1">
                     <div className="items-center justify-center flex flex-col">
-                        <h1 className="text-[#1F306A] font-bold mb-2">นายกสภาวิทยาลัย</h1>
+                        <h1 className="text-[#2797FF] font-bold mb-2">นายกสภาวิทยาลัย</h1>
                         <img src="https://static.wikia.nocookie.net/heroes-and-villain/images/8/8b/Snape_%281%29.jpg" alt="" />
-                        <h5 className="text-[#536878] text-sm mt-2 ">ศาสตราจารย์ ดร.บุญทัน ดอกไธสง</h5>
+                        <h5 className="text-white text-sm mt-2 ">ศาสตราจารย์ ดร.บุญทัน ดอกไธสง</h5>
                     </div>
                 </div>
                 {/*  */}
@@ -145,56 +150,56 @@ export default function About() {
                 {/* กรรมการสภาวิทยาลัย อธิการบดี  */}
                 <div className="columns-1">
                     <div className="items-center justify-center flex flex-col mt-10">
-                        <h1 className="text-[#1F306A] font-bold mb-2">กรรมการสภาวิทยาลัย อธิการบดี</h1>
+                        <h1 className="text-[#2797FF] font-bold mb-2">กรรมการสภาวิทยาลัย อธิการบดี</h1>
                         <img src="https://static.wikia.nocookie.net/heroes-and-villain/images/8/8b/Snape_%281%29.jpg" alt="" />
-                        <h5 className="text-[#536878] text-sm mt-2 ">ดร.พิสิษฐ์ ศิริรักษ์</h5>
+                        <h5 className="text-white text-sm mt-2 ">ดร.พิสิษฐ์ ศิริรักษ์</h5>
                     </div>
                 </div>
                 {/*  */}
 
                 {/* กรรมการสภาวิทยาลัย  */}
-                <h1 className="text-[#1F306A] font-bold mb-2 text-center py-12">กรรมการสภาวิทยาลัย</h1>
+                <h1 className="text-[#2797FF] font-bold mb-2 text-center py-12">กรรมการสภาวิทยาลัย</h1>
                 
                 <div className="columns-1 md:columns-4">
                     <div className="items-center justify-center flex flex-col ">
                         <img src="https://static.wikia.nocookie.net/heroes-and-villain/images/8/8b/Snape_%281%29.jpg" alt="" />
-                        <h5 className="text-[#536878] text-sm mt-2 ">ดร.อินทร์ จันทร์เจริญ</h5>
+                        <h5 className="text-white text-sm mt-2 ">ดร.อินทร์ จันทร์เจริญ</h5>
                     </div>
 
                     <div className="items-center justify-center flex flex-col mt-10">
                         <img src="https://static.wikia.nocookie.net/heroes-and-villain/images/8/8b/Snape_%281%29.jpg" alt="" />
-                        <h5 className="text-[#536878] text-sm mt-2 ">ศ.ดร.จุลละพงษ์ จุลละโพธิ์</h5>
+                        <h5 className="text-white text-sm mt-2 ">ศ.ดร.จุลละพงษ์ จุลละโพธิ์</h5>
                     </div>
 
                     <div className="items-center justify-center flex flex-col mt-10">
                         <img src="https://static.wikia.nocookie.net/heroes-and-villain/images/8/8b/Snape_%281%29.jpg" alt="" />
-                        <h5 className="text-[#536878] text-sm mt-2 ">ดร.ทิวา พงษ์ธนไพบูลย์</h5>
+                        <h5 className="text-white text-sm mt-2 ">ดร.ทิวา พงษ์ธนไพบูลย์</h5>
                     </div>
 
                     <div className="items-center justify-center flex flex-col mt-10">
                         <img src="https://static.wikia.nocookie.net/heroes-and-villain/images/8/8b/Snape_%281%29.jpg" alt="" />
-                        <h5 className="text-[#536878] text-sm mt-2 ">รอง ศ.ดร.ชัยยุทธ ช่างสาร</h5>
+                        <h5 className="text-white text-sm mt-2 ">รอง ศ.ดร.ชัยยุทธ ช่างสาร</h5>
                     </div>
 
 
                     <div className="items-center justify-center flex flex-col ">
                         <img src="https://static.wikia.nocookie.net/heroes-and-villain/images/8/8b/Snape_%281%29.jpg" alt="" />
-                        <h5 className="text-[#536878] text-sm mt-2 ">ผู้ช่วย ศ.ฉัตรชัย เธียรหิรัญ</h5>
+                        <h5 className="text-white text-sm mt-2 ">ผู้ช่วย ศ.ฉัตรชัย เธียรหิรัญ</h5>
                     </div>
 
                     <div className="items-center justify-center flex flex-col mt-10">
                         <img src="https://static.wikia.nocookie.net/heroes-and-villain/images/8/8b/Snape_%281%29.jpg" alt="" />
-                        <h5 className="text-[#536878] text-sm mt-2 ">ดร.วราภรณ์ สีหนาท</h5>
+                        <h5 className="text-white text-sm mt-2 ">ดร.วราภรณ์ สีหนาท</h5>
                     </div>
 
                     <div className="items-center justify-center flex flex-col mt-10">
                         <img src="https://static.wikia.nocookie.net/heroes-and-villain/images/8/8b/Snape_%281%29.jpg" alt="" />
-                        <h5 className="text-[#536878] text-sm mt-2 ">ดร.รอง ศ.ดร.อัญชลี สารรัตนะ</h5>
+                        <h5 className="text-white text-sm mt-2 ">ดร.รอง ศ.ดร.อัญชลี สารรัตนะ</h5>
                     </div>
 
                     <div className="items-center justify-center flex flex-col mt-10">
                         <img src="https://static.wikia.nocookie.net/heroes-and-villain/images/8/8b/Snape_%281%29.jpg" alt="" />
-                        <h5 className="text-[#536878] text-sm mt-2 ">รอง ศ.ดร.ปพฤกษ์ อุตสาหะวาณิชกิจ</h5>
+                        <h5 className="text-white text-sm mt-2 ">รอง ศ.ดร.ปพฤกษ์ อุตสาหะวาณิชกิจ</h5>
                     </div>
 
 
@@ -206,9 +211,9 @@ export default function About() {
                 {/* เลขานุการสภาวิทยาลัย  */}
                 <div className="columns-1">
                     <div className="items-center justify-center flex flex-col mt-20 ">
-                        <h1 className="text-[#1F306A] font-bold mb-2">เลขานุการสภาวิทยาลัย</h1>
+                        <h1 className="text-[#2797FF] font-bold mb-2">เลขานุการสภาวิทยาลัย</h1>
                         <img src="https://static.wikia.nocookie.net/heroes-and-villain/images/8/8b/Snape_%281%29.jpg" alt="" />
-                        <h5 className="text-[#536878] text-sm mt-2 ">ผู้ช่วย ศ.กุลธนี ศิริรักษ์</h5>
+                        <h5 className="text-white text-sm mt-2 ">ผู้ช่วย ศ.กุลธนี ศิริรักษ์</h5>
                     </div>
                 </div>
                 {/*  */}
@@ -218,9 +223,9 @@ export default function About() {
                 {/* กรรมการสภาวิทยาลัย อธิการบดี  */}
                 <div className="columns-1">
                     <div className="items-center justify-center flex flex-col mt-10">
-                        <h1 className="text-[#1F306A] font-bold mb-2">ผู้ช่วยเลขานุการสภามหาวิทยาลัย</h1>
+                        <h1 className="text-[#2797FF] font-bold mb-2">ผู้ช่วยเลขานุการสภามหาวิทยาลัย</h1>
                         <img src="https://static.wikia.nocookie.net/heroes-and-villain/images/8/8b/Snape_%281%29.jpg" alt="" />
-                        <h5 className="text-[#536878] text-sm mt-2 ">ผู้ช่วย ศ.ดร.สมพร เจนคุณาวัฒน์</h5>
+                        <h5 className="text-white text-sm mt-2 ">ผู้ช่วย ศ.ดร.สมพร เจนคุณาวัฒน์</h5>
                     </div>
                 </div>
                 {/*  */}
@@ -232,7 +237,19 @@ export default function About() {
 
             {/* div container */}
             </div>
+            {/* ปิด div พื้นหลังใหญ่ */}
+            </div>
+            {/* แทบ รู้จักพนมวันท์ */}
+            <div className=''>
+                    <div className="bg-[#1F306A] bg-opacity-80 flex justify-center items-center">
+                        <h1 className='md:text-3xl text-white text-center py-10'>
+                        ติดต่อขอคำปรึกษา 
+                        </h1>
+                        <button className="bg-[#FFBF00] rounded-lg text-white ml-1 w-14 md:w-24  md:text-3xl">คลิก</button>
+                    
+                    </div>
 
+            </div>
         </RootLayout>
     );
 }
