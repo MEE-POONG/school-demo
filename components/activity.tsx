63daaaf6d1562faf88c73e8dec2f0223f525cb2f
devilzeros <devilzeros@outlook.co.th>
2023-07-31 00:07:09 +0700

    est

--- components/activity.tsx
+++ components/activity.tsx
@@ -1,19 +1,19 @@
 import Link from "next/link";
 export default function Activity() {
   return (
-    <div>
+    <div className="py-5 mb-5">
       <div className=" bg-[#1F306A] p-8 md:h-150 lg:h-190 container mx-auto items-center justify-center mb-5">
-        <h1 className=" text-lg md:text-3xl  md:ml-0 mt-1 text-center text-yellow-400">
+        <h1 className=" text-lg md:text-3xl  md:ml-0 py-5 text-center text-yellow-400">
           กิจกรรมที่ผ่านมา
         </h1>
-        <div className="grid grid-cols-3 gap-3 mt-5 container mx-auto">
-          <Link href="">
+        <div className="grid grid-cols-3 gap-3 container mx-auto">
+          <Link href="" className="p-4">
             <img className="" src="/img/event/event1.png" />
           </Link>
-          <Link href="">
+          <Link href="" className="p-4">
             <img src="/img/event/event6.png" />
           </Link>
-          <Link href="">
+          <Link href="" className="p-4">
             <img src="/img/event/event3.png" />
           </Link>
         </div>
@@ -21,13 +21,8 @@ export default function Activity() {
       <div className="md:w-full h-0.5 mt-4 bg-gradient-to-r from-blue-900 via-yellow-500 to-blue-900  md:h-150 lg:h-190 container mx-auto items-center justify-center mb-5"></div>
 
       <div className="text-center min-h-[140px] w-full place-items-center overflow-x-scroll rounded-lg p-1 lg:overflow-visible ">
-        <Link href="news">
-          <button
-            className=" underline underline-offset-1 middle none center mr-4 rounded-lg bg-blue-900 py-3 px-6 font-sans text-lg font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-            data-ripple-light="true"
-          >
-            อ่านทั้งหมด
-          </button>
+        <Link href="news" className=" underline underline-offset-1 middle none center mr-4 rounded-lg bg-blue-900 py-3 px-5 font-sans text-lg font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+          อ่านทั้งหมด
         </Link>
       </div>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,17 +18,10 @@ const inter = Inter({ subsets: ["latin"] });
 export default function About() {
   return (
     <RootLayout>
-     
-      <Slider/>
-
-        <Menu/>
-      
-
-
-
+      <Slider />
+      <Menu />
       <div className="md:w-full h-1  mb-7 bg-gradient-to-r from-blue-900 via-yellow-500 to-blue-900"></div>
       <div className="container mx-auto  md:py-10 py-2 text-center">
-
         <span className=" text-2xl md:text-4xl">
           <span className=" text-blue-800 ">ข่าว</span>
           <span className=" text-yellow-500">“พนมวันท์”</span>
@@ -38,12 +31,8 @@ export default function About() {
           <Slider />
         </div>
       </div>
-
-      <Activity/>
-      
-      
-
-      <div className="container mx-auto  md:py-2 py-2  ">
+      <Activity />
+      <div className="container mx-auto  md:py-10 py-2  ">
         <div className="flex justify-center items-center ">
           <div className="bg-blue-900 w-20 h-0.5"></div>
           <div className=" text-blue-900 text-xl md:text-2xl mx-2">สถิติ</div>
@@ -51,8 +40,22 @@ export default function About() {
         </div>
       </div>
       <div className=" bg-slate-800 w-full h-48 md:h-full">
-        <div className="md:grid md:grid-flow-col md:auto-cols-max md:gap-2 md:justify-center h-20 text-center">
-          <div className=" my-auto text-2xl text-yellow-400 md:border-r-2 border-gray-500">
+        <div className="md:grid md:grid-flow-col md:auto-cols-max md:gap-2 md:justify-center h-36 text-center">
+          <div className="flex my-auto text-2xl px-5 text-yellow-400 md:border-r-2 border-gray-500 text-6xl	">
+            4<span className="my-auto text-2xl text-white mr-2">&nbsp;คณะ</span>
+          </div>
+          <div className="flex my-auto text-2xl px-5 text-yellow-400 md:border-r-2 border-gray-500 text-6xl	">
+            20<span className="my-auto text-2xl text-white mr-2">&nbsp;หลักสูตร</span>
+          </div>
+          <div className="flex my-auto text-2xl px-5 text-yellow-400 md:border-r-2 border-gray-500 text-6xl	">
+            500<span className="my-auto text-2xl text-white mr-2">&nbsp;กำลังศึกษา</span>
+          </div>
+          <div className="flex my-auto text-2xl px-5 text-yellow-400 text-6xl	">
+            1000<span className="my-auto text-2xl text-white mr-2">&nbsp;ผู้เข้าชมเว็บไซต์</span>
+          </div>
+        </div>
+        {/* <div className="md:grid md:grid-flow-col md:auto-cols-max md:gap-2 md:justify-center h-36 text-center">
+          <div className=" my-auto text-2xl text-yellow-400 md:border-r-2 border-gray-500 ">
             4<span className="my-auto text-xl text-white mr-2">&nbsp;คณะ</span>
           </div>
           <div className=" my-auto text-2xl text-yellow-400 md:border-r-2 border-gray-500">
@@ -79,7 +82,7 @@ export default function About() {
               &nbsp;ผู้เข้าชมเว็บไซต์
             </span>
           </div>
-        </div>
+        </div> */}
       </div>
     </RootLayout>
   );
