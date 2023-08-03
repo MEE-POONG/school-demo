acc8ccf3310496dbd08fdd6b81941e990a8cac8a
ChonracahtPond <137247404+ChonracahtPond@users.noreply.github.com>
2023-08-03 14:16:41 +0700

    pond

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,5 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import RootLayout from "../../components/layout";
-import Footer from "@/components/footer";
-import Navbar from "@/components/navbar";
-import { RiFilePaper2Line } from "react-icons/ri";
-import { GrMoney } from "react-icons/gr";
-import { RiGraduationCapLine } from "react-icons/ri";
-import { FaUserGraduate } from "react-icons/fa";
-import Link from "next/link";
-import { FaMoneyBill } from "react-icons/fa";
 import Menu from "@/container/menu";
 import Activity from "@/components/activity";
 import Slider from "@/components/slider";
