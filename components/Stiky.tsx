b5bf9bd6db4a020e71e4a3709ef2f35554ff467c
ChonracahtPond <137247404+ChonracahtPond@users.noreply.github.com>
2023-08-04 17:21:16 +0700

    pond

---
+++ components/Stiky.tsx
@@ -0,0 +1,11 @@
+export default function Stikey(){
+    return(
+        <div className="fixed bottom-[150px]  left-0 right-5 z-40 px-4 py-2 ">
+      
+      <a className="text-gray-400 underline"> <img
+            className=" absolute inset-y-0 right-0 w-16"
+            src="/img/messenger-Logo.png"
+          /></a>
+    </div>
+    )
+}
--- components/layout.tsx
+++ components/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 import React from "react";
 import Footer from './footer';
 import ComponentsNavbar from './navbar';
+import Stikey from './Stiky';
  
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,16 +13,18 @@ export default function RootLayout({
 }) {
   return (
     <>
-   <div className="fixed bottom-[150px]  left-0 right-5 z-40 px-4 py-2 ">
+   {/* <div className="fixed bottom-[150px]  left-0 right-5 z-40 px-4 py-2 ">
       
       <a className="text-gray-400 underline"> <img
             className=" absolute inset-y-0 right-0 w-16"
             src="/img/messenger-Logo.png"
           /></a>
-    </div>
+    </div> */}
       <ComponentsNavbar/>
       
+      
       <main className={inter.className}>{children}
+      <Stikey/>
       </main>
       
     
