7194d9fb087099ff232047cf2a9c279eab6eca05
ChonracahtPond <137247404+ChonracahtPond@users.noreply.github.com>
2023-08-04 15:18:09 +0700

    pond

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,10 +7,8 @@ const Footer = () => {
   return (
     <footer className=" bg-[#1F306A] w-full font-fontTH">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex flex-col md:flex-row items-center justify-between"> 
-    
-          <LogoPanomwan2/>
-          
+        <div className="flex flex-col md:flex-row items-center justify-between">
+          <LogoPanomwan2 />
 
           <div className="md:flex  text-gray-500 my-8">
             <div className="md:mr-10">
@@ -19,34 +17,22 @@ const Footer = () => {
               </h2>
               <ul className="text-gray-500">
                 <li className="mb-4">
-                  <Link
-                    href=""
-                    className="hover:underline text-white"
-                  >
+                  <Link href="" className="hover:underline text-white">
                     คณะบริหารธรุกิจ
                   </Link>
                 </li>
                 <li className="mb-4">
-                  <Link
-                    href=""
-                    className="hover:underline text-white"
-                  >
+                  <Link href="" className="hover:underline text-white">
                     คณะวิทยาศาสตร์และเทคโนโลยี
                   </Link>
                 </li>
                 <li className="mb-4">
-                  <Link
-                    href=""
-                    className="hover:underline text-white"
-                  >
+                  <Link href="" className="hover:underline text-white">
                     คณะวิทยาศาสตร์
                   </Link>
                 </li>
                 <li>
-                  <Link
-                    href=""
-                    className="hover:underline text-white"
-                  >
+                  <Link href="" className="hover:underline text-white">
                     คณะศิลปศาสตร์
                   </Link>
                 </li>
@@ -54,31 +40,20 @@ const Footer = () => {
             </div>
 
             <div className="">
-            <h2 className="mb-6 text-md font-semibold text-white">
-                รายการ
-              </h2>
+              <h2 className="mb-6 text-md font-semibold text-white">รายการ</h2>
               <ul className="text-white">
                 <li className="mb-4">
-                  <Link
-                    href=""
-                    className="hover:underline "
-                  >
+                  <Link href="" className="hover:underline ">
                     สมัครเรียน
                   </Link>
                 </li>
                 <li className="mb-4">
-                  <Link
-                    href=""
-                    className="hover:underline "
-                  >
+                  <Link href="" className="hover:underline ">
                     ปรึกษาเรื่องทุน
                   </Link>
                 </li>
                 <li className="mb-4">
-                  <Link
-                    href=""
-                    className="hover:underline "
-                  >
+                  <Link href="" className="hover:underline ">
                     ปรึกษาเรื่องสมัครเรียน
                   </Link>
                 </li>
@@ -89,20 +64,16 @@ const Footer = () => {
       </div>
 
       <div className=" bg-black w-full ">
-      <span className=" text-white font-serif sm:text-sm">
-    <p className=" text-center">
-      <a href="">© 2023{" "}  Me Prompt Technology CONPANY LIMITED.  All Rights Reserved.</a>
-    </p>
-    </span>
+        <span className=" text-white font-serif sm:text-sm">
+          <p className=" text-center">
+            <a href="">
+              © 2023 Me Prompt Technology CONPANY LIMITED. All Rights Reserved.
+            </a>
+          </p>
+        </span>
       </div>
     </footer>
-
-    
   );
 };
 
 export default Footer;
-
-
-
-
--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -1,184 +1,184 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import RootLayout from '@/components/layout'
-import { BsBuildingFill } from "react-icons/bs";
-import { IoNewspaperSharp } from "react-icons/io5";
-import { PiHandCoinsBold } from "react-icons/pi";
-import { GoBook } from "react-icons/go";
-import { MdSupportAgent } from "react-icons/md";
-import { TbStars } from "react-icons/tb";
-import { FaLocationDot } from "react-icons/fa6";
+import { Inter } from "next/font/google";
+import RootLayout from "@/components/layout";
+import Link from "next/link";
 
-
-
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Welfare() {
-    return (
-        <RootLayout>
-
-        {/* banner คณะและหลักสูตร */}
-        <div className="relative">
-            <img src="/img/news/building2.png" alt="" />
-                <div className="absolute inset-0  mb-2 md:mb-24  flex justify-center items-center">
-                    {/* <h1 className='text-white text-xl md:text-6xl lg:text-7xl '>
+  return (
+    <RootLayout>
+      {/* banner คณะและหลักสูตร */}
+      <div className="relative">
+        <img src="/img/news/building2.png" alt="" />
+        <div className="absolute inset-0  mb-2 md:mb-24  flex justify-center items-center">
+          {/* <h1 className='text-white text-xl md:text-6xl lg:text-7xl '>
                     วิทยาลัยเทคโนโลยีพนมวันท์
                     </h1>    */}
-                </div>
-            </div>
-
-    
-        {/* แทบ ข่าวประชาสัมพันธ์ */}
-        <div className=''>
-                    <div className="bg-black bg-opacity-80">
-                        <h1 className='text-xl md:text-3xl text-[#FFBF00] text-center py-4 md:py-6'>
-                            ข่าวประชาสัมพันธ์
-                        </h1>
-                    
-                    </div>
-
         </div>
+      </div>
 
-        {/* เนื้อหาข่าวประชาสัมพันธ์ */}
-        <div className='bg-[#E5E4E2]'>
-            {/* คอลั่ม */}
-            <div className=' container mx-auto'>
-            <div className='grid grid-cols-2 md:grid-cols-4'>
+      {/* แทบ ข่าวประชาสัมพันธ์ */}
+      <div className="">
+        <div className="bg-black bg-opacity-80">
+          <h1 className="text-xl md:text-3xl text-[#FFBF00] text-center py-4 md:py-6">
+            ข่าวประชาสัมพันธ์
+          </h1>
+        </div>
+      </div>
+
+      {/* เนื้อหาข่าวประชาสัมพันธ์ */}
+      <div className="bg-[#E5E4E2]">
+        {/* คอลั่ม */}
+        <div className=" container mx-auto">
+          <div className="grid grid-cols-2 md:grid-cols-4">
             {/* ข้อมูลในคอลั่ม */}
 
+            <div className="p-3">
+              <div className="box-content shadow-xl rounded-lg ">
+                <img
+                  className="rounded-lg"
+                  src="../img/banner/banner4.png"
+                  alt=""
+                />
+                <h1 className="text-[#1F306A] px-2 pt-2 h-14 text-sm truncate ">
+                  เปิดรับสมัครนักศึกษาใหม่ 2566
+                </h1>
+              </div>
+            </div>
 
-                <div className='p-5'>
-                    <div className='box-content shadow-xl rounded-lg '>
-                        <img className='rounded-lg' src="/img/imgSlider/school7.jpg" alt="" />
-                        <h1 className='text-[#1F306A] px-2 pt-2 h-14 text-sm truncate '>
-                        เปิดรับสมัครนักศึกษาใหม่ 2566
+            <div className="p-3">
+              <div className="box-content shadow-xl rounded-lg ">
+                <img
+                  className="rounded-lg"
+                  src="../img/banner/banner2.png"
+                  alt=""
+                />
+                <h1 className="text-[#1F306A] px-2 pt-2 h-14 text-sm truncate ">
+                  กู้ กยศ. 100% สำหรับนักเรียน นักศึกษา
+                </h1>
+              </div>
+            </div>
 
-                        </h1>
-                    </div>
-                </div>
+            <div className="p-3">
+              <div className="box-content shadow-xl rounded-lg ">
+                <img
+                  className="rounded-lg"
+                  src="../img/banner/banner1.png"
+                  alt=""
+                />
+                <h1 className="text-[#1F306A] px-2 pt-2 h-14 text-sm truncate ">
+                  ให้คำปรึกษาโดยอาจารย์ผู้เชี่ยวชาญ
+                </h1>
+              </div>
+            </div>
 
-
-                <div className='p-5'>
-                    <div className='box-content shadow-xl rounded-lg '>
-                        <img className='rounded-lg' src="/img/imgSlider/school7.jpg" alt="" />
-                        <h1 className='text-[#1F306A] px-2 pt-2 h-14 text-sm truncate '>
-                        เปิดรับสมัครนักศึกษาใหม่ 2566
-
-                        </h1>
-                    </div>
-                </div>
-
-
-                <div className='p-5'>
-                    <div className='box-content shadow-xl rounded-lg '>
-                        <img className='rounded-lg' src="/img/imgSlider/school7.jpg" alt="" />
-                        <h1 className='text-[#1F306A] px-2 pt-2 h-14 text-sm truncate '>
-                        เปิดรับสมัครนักศึกษาใหม่ 2566
-
-                        </h1>
-                    </div>
-                </div>
-
-
-                <div className='p-5'>
-                    <div className='box-content shadow-xl rounded-lg '>
-                        <img className='rounded-lg' src="/img/imgSlider/school7.jpg" alt="" />
-                        <h1 className='text-[#1F306A] px-2 pt-2 h-14 text-sm truncate '>
-                        เปิดรับสมัครนักศึกษาใหม่ 2566
-
-                        </h1>
-                    </div>
-                </div>
-
+            <div className="p-3">
+              <div className="box-content shadow-xl rounded-lg ">
+                <img
+                  className="rounded-lg"
+                  src="../img/banner/bannerwts.png"
+                  alt=""
+                />
+                <h1 className="text-[#1F306A] px-2 pt-2 h-14 text-sm truncate ">
+                  หลักสูตร เสาร์-อาทิตย์ เรียนออนไลน์ 100%
+                </h1>
+              </div>
+            </div>
 
             {/* ปิดคอลั่ม */}
-            </div>
-            </div>
+          </div>
         </div>
+      </div>
 
+      {/* ปุ่มโหลดเพิ่มเติม 1 */}
+      <div className=" py-8 flex justify-center items-center">
+        {/* <button className='bg-black bg-opacity-80 text-white rounded-lg  drop-shadow-lg h-12 w-28'>โหลดเพิ่มเติม</button> */}
+        <button className="group relative h-12 w-40 overflow-hidden rounded-2xl bg-black bg-opacity-80 text-lg font-bold text-white">
+        โหลดเพิ่มเติม
+          <div className="absolute inset-0 h-full w-full scale-0 rounded-2xl transition-all duration-300 group-hover:scale-100 group-hover:bg-white/30" />
+        </button>
+      </div>
 
-        {/* ปุ่มโหลดเพิ่มเติม 1 */}
-        <div className=' py-8 flex justify-center items-center'>
-            <button className='bg-black bg-opacity-80 text-white rounded-lg  drop-shadow-lg h-12 w-28'>โหลดเพิ่มเติม</button>
+      {/* แทบ ข่าวกิจกรรม */}
+      <div className="">
+        <div className="bg-black bg-opacity-80">
+          <h1 className="text-xl md:text-3xl text-[#FFBF00] text-center py-4 md:py-6">
+            ข่าวกิจกรรม
+          </h1>
         </div>
+      </div>
 
-        {/* แทบ ข่าวกิจกรรม */}
-        <div className=''>
-                    <div className="bg-black bg-opacity-80">
-                        <h1 className='text-xl md:text-3xl text-[#FFBF00] text-center py-4 md:py-6'>
-                            ข่าวกิจกรรม
-                        </h1>
-                    
-                    </div>
-
-        </div>
-
-
-        {/* เนื้อหาข่าวกิจกรรม */}
-        <div className='bg-[#E5E4E2]'>
-        <div className=' container mx-auto'>
-                    {/* คอลั่ม */}
-            <div className='grid grid-cols-2 md:grid-cols-4'>
+      {/* เนื้อหาข่าวกิจกรรม */}
+      <div className="bg-[#E5E4E2]">
+        <div className=" container mx-auto">
+          {/* คอลั่ม */}
+          <div className="grid grid-cols-2 md:grid-cols-4">
             {/* ข้อมูลในคอลั่ม */}
 
-                <div className='p-5'>
-                    <div className='box-content shadow-xl rounded-lg '>
-                        <img className='rounded-lg' src="/img/imgSlider/school7.jpg" alt="" />
-                        <h1 className='text-[#1F306A] px-2 pt-2 h-14 text-sm truncate '>
-                        เปิดรับสมัครนักศึกษาใหม่ 2566
+            <div className="p-5">
+              <div className="box-content shadow-xl rounded-lg ">
+                <img
+                  className="rounded-lg"
+                  src="/img/imgSlider/school7.jpg"
+                  alt=""
+                />
+                <h1 className="text-[#1F306A] px-2 pt-2 h-14 text-sm truncate ">
+                  เปิดรับสมัครนักศึกษาใหม่ 2566
+                </h1>
+              </div>
+            </div>
 
-                        </h1>
-                    </div>
-                </div>
+            <div className="p-5">
+              <div className="box-content shadow-xl rounded-lg ">
+                <img
+                  className="rounded-lg"
+                  src="/img/imgSlider/school7.jpg"
+                  alt=""
+                />
+                <h1 className="text-[#1F306A] px-2 pt-2 h-14 text-sm truncate ">
+                  เปิดรับสมัครนักศึกษาใหม่ 2566
+                </h1>
+              </div>
+            </div>
 
+            <div className="p-5">
+              <div className="box-content shadow-xl rounded-lg ">
+                <img
+                  className="rounded-lg"
+                  src="/img/imgSlider/school7.jpg"
+                  alt=""
+                />
+                <h1 className="text-[#1F306A] px-2 pt-2 h-14 text-sm truncate ">
+                  เปิดรับสมัครนักศึกษาใหม่ 2566
+                </h1>
+              </div>
+            </div>
 
-                <div className='p-5'>
-                    <div className='box-content shadow-xl rounded-lg '>
-                        <img className='rounded-lg' src="/img/imgSlider/school7.jpg" alt="" />
-                        <h1 className='text-[#1F306A] px-2 pt-2 h-14 text-sm truncate '>
-                        เปิดรับสมัครนักศึกษาใหม่ 2566
-
-                        </h1>
-                    </div>
-                </div>
-
-
-                <div className='p-5'>
-                    <div className='box-content shadow-xl rounded-lg '>
-                        <img className='rounded-lg' src="/img/imgSlider/school7.jpg" alt="" />
-                        <h1 className='text-[#1F306A] px-2 pt-2 h-14 text-sm truncate '>
-                        เปิดรับสมัครนักศึกษาใหม่ 2566
-
-                        </h1>
-                    </div>
-                </div>
-
-
-                <div className='p-5'>
-                    <div className='box-content shadow-xl rounded-lg '>
-                        <img className='rounded-lg' src="/img/imgSlider/school7.jpg" alt="" />
-                        <h1 className='text-[#1F306A] px-2 pt-2 h-14 text-sm truncate '>
-                        เปิดรับสมัครนักศึกษาใหม่ 2566
-
-                        </h1>
-                    </div>
-                </div>
-
+            <div className="p-5">
+              <div className="box-content shadow-xl rounded-lg ">
+                <img
+                  className="rounded-lg"
+                  src="/img/imgSlider/school7.jpg"
+                  alt=""
+                />
+                <h1 className="text-[#1F306A] px-2 pt-2 h-14 text-sm truncate ">
+                  เปิดรับสมัครนักศึกษาใหม่ 2566
+                </h1>
+              </div>
+            </div>
 
             {/* ปิดคอลั่ม */}
-            </div>
-            </div>
+          </div>
         </div>
+      </div>
 
-
-        {/* ปุ่มโหลดเพิ่มเติม 2 */}
-        <div className=' py-8 flex justify-center items-center'>
-            <button className='bg-black bg-opacity-80 text-white rounded-lg  drop-shadow-lg h-12 w-28'>โหลดเพิ่มเติม</button>
-        </div>
-
-
-        </RootLayout>
-
-    )
+      {/* ปุ่มโหลดเพิ่มเติม 2 */}
+      <div className=" py-8 flex justify-center items-center">
+      <button className="group relative h-12 w-40 overflow-hidden rounded-2xl bg-black bg-opacity-80 text-lg font-bold text-white">
+        โหลดเพิ่มเติม
+          <div className="absolute inset-0 h-full w-full scale-0 rounded-2xl transition-all duration-300 group-hover:scale-100 group-hover:bg-white/30" />
+        </button>
+      </div>
+    </RootLayout>
+  );
 }
