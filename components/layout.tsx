1acd32de396c33a525e039e0bf2606f431a639b1
wechanon <wechanon2001@gmail.com>
2023-08-04 09:53:34 +0700

    tiw

--- components/layout.tsx
+++ components/layout.tsx
@@ -19,8 +19,8 @@ export default function RootLayout({
       <ComponentsNavbar/>
       <StickyBox offsetTop={10} offsetBottom={10}>
             <Link href="https://www.facebook.com/PhanomwanCollege/" >
-                <div className=" relative ">
-            <img className=" w-14 absolute right-8 top-1 bottom-auto " src="/img/messenger-Logo.png"/>
+                <div className="relative ">
+            <img className=" absolute inset-y-0 right-0 w-16" src="/img/messenger-Logo.png"/>
             </div>
             </Link>
            </StickyBox>
