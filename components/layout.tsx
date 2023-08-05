bd615e60f6671fadbc3c483adc741b3786383910
ChonracahtPond <137247404+ChonracahtPond@users.noreply.github.com>
2023-08-05 10:46:14 +0700

    pond

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,35 +1,49 @@
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
 import React from "react";
-import Footer from './footer';
-import ComponentsNavbar from './navbar';
-import Stikey from './Stiky';
- 
-const inter = Inter({ subsets: ['latin'] })
+import Footer from "./footer";
+import ComponentsNavbar from "./navbar";
+
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <>
-   {/* <div className="fixed bottom-[150px]  left-0 right-5 z-40 px-4 py-2 ">
+  
+    
+      <ComponentsNavbar />
+
+      <main className={inter.className}> 
+      {children}
+      
+      </main>
+    
+      <Footer />
+    </>
+  );
+}
+
+
+
+
+
+  {/* <Stikey/> */}
+
+
+      {/* <div className="fixed bottom-[150px] ">
+        <a href="//">
+          <img src="/img/messenger-Logo.png" className=" absolute w-16 right-0" alt="" />
+        </a>
+      </div> */}
+
+      {/* <div className="fixed bottom-[150px]  left-0 right-5 z-40 px-4 py-2 ">
       
       <a className="text-gray-400 underline"> <img
-            className=" absolute inset-y-0 right-0 w-16"
+            className="absolute inset-y-0 right-0 w-16 "
             src="/img/messenger-Logo.png"
           /></a>
     </div> */}
-      <ComponentsNavbar/>
-      
-      
-      <main className={inter.className}>{children}
-      <Stikey/>
-      </main>
-      
-    
-      
-      <Footer />
-    </>
-  )
-}
