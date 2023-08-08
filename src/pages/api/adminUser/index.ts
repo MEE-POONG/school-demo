3ead7f5142e43f3f2aaf7ab10de91e14315b673e
ChonracahtPond <137247404+ChonracahtPond@users.noreply.github.com>
2023-08-08 09:40:04 +0700

    pond

--- src/pages/api/adminUser/index.ts
+++ src/pages/api/adminUser/index.ts
@@ -12,28 +12,28 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 const page: number = Number(req.query.page) || 1;
                 const pageSize: number = Number(req.query.pageSize) || 10;
 
-                const welfare = await prisma.welfare.findMany({
+                const adminUser = await prisma.adminUser.findMany({
                     skip: (page - 1) * pageSize,
                     take: pageSize,
                 });
 
-                const totalwelfare = await prisma.welfare.count();
-                const totalPage: number = Math.ceil(totalwelfare / pageSize);
-                res.status(200).json({ welfare });
+                const totaladminUser = await prisma.adminUser.count();
+                const totalPage: number = Math.ceil(totaladminUser / pageSize);
+                res.status(200).json({ adminUser });
             } catch (error) {
-                res.status(500).json({ error: "An error occurred while fetching the welfare" });
+                res.status(500).json({ error: "An error occurred while fetching the adminUser" });
             }
             break;
 
         case 'POST':
             try {
-                const newwelfare = await prisma.welfare.create({
+                const newadminUser = await prisma.adminUser.create({
                     data: req.body,
                 });
 
-                res.status(201).json(newwelfare);
+                res.status(201).json(newadminUser);
             } catch (error) {
-                res.status(500).json({ error: "An error occurred while creating the welfare" });
+                res.status(500).json({ error: "An error occurred while creating the adminUser" });
             }
             break;
 
