37959a6a5c1055ff5bb81d5afe4db96c52ef2c09
ken1lnw <moowinza@gmail.com>
2023-08-10 09:25:29 +0700

    form

---
+++ src/pages/api/upload/index.ts
@@ -0,0 +1,55 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export const config = {
+    api: {
+      bodyParser: {
+        sizeLimit: '4mb' // Set desired value here
+      }
+    }
+  };
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const apiUrl = 'https://api.cloudflare.com/client/v4/accounts/39aa4ea3c7a7d766adc4428933324787/images/v1';
+  const token = 'LpMNSFUw7gmxpn4ZZ7P2ZAcReF6Q-HlbIWqthbO0';
+
+  if (req.method === 'GET') {
+    try {
+      const response = await fetch(apiUrl, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        res.status(200).json(data);
+      } else {
+        const errorData = await response.json();
+        res.status(response.status).json(errorData);
+      }
+    } catch (error) {
+      res.status(500).json({ error: 'An error occurred' });
+    }
+  } else if (req.method === 'POST') {
+    try {
+      // Assuming you're sending a file via a form with "file" field
+      const file = req.body.file; // Adjust this based on your form field name
+      // Handle file upload logic using the "file" variable
+      
+      // Example: Saving the file to a local directory
+      // const fs = require('fs');
+      // fs.writeFileSync('path/to/save/file', file);
+
+      res.status(200).send(file);
+      
+    } catch (error) {
+      res.status(500).json({ error: 'An error occurred' });
+    }
+  } else if (req.method === 'DELETE') {
+    // Handle DELETE logic here if needed
+    res.status(405).json({ error: 'Method not allowed' });
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+}
