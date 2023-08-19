3b7bbb039476fbc3804957d4f1cf6f9e3b6bca7a
wechanon <wechanon2001@gmail.com>
2023-08-19 11:08:42 +0700

    tiw

--- src/pages/news/news1.tsx
+++ src/pages/news/news1.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import RootLayout from '@/components/layout';
 import Link from 'next/link';
-import { Head } from 'next/document';
 
 const NewsArticle: React.FC = () => {
   return (
