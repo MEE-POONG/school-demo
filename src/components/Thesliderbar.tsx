c76901a47b05705f3d130a1586d86c75f535fcd5
ken1lnw <moowinza@gmail.com>
2023-08-29 10:17:11 +0700

    loading

--- src/components/Thesliderbar.tsx
+++ src/components/Thesliderbar.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import Loading from './loading';
 
 interface sliderSchool {
   id: number;
@@ -12,15 +13,20 @@ const IndexPage: React.FC = () => {
   const [sliderSchoolData, setsliderSchoolData] = useState<sliderSchool[]>([]); // Specify the type here
   const [sliderRef, setSliderRef] = useState<Slider | null>(null);
   const [activeSlide, setActiveSlide] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch('/api/sliderSchool')
       .then((response) => response.json())
       .then((data) => {
         setsliderSchoolData(data.sliderSchool);
+        setIsLoading(false); // ตั้งค่า isLoading เป็น false เมื่อโหลดเสร็จสมบูรณ์
+
       })
       .catch((error) => {
         console.error('Error:', error);
+        setIsLoading(false); // ตั้งค่า isLoading เป็น false เมื่อโหลดเสร็จสมบูรณ์
+
       });
   }, []);
 
@@ -48,11 +54,12 @@ const IndexPage: React.FC = () => {
 
   return (
     <div style={{ position: 'relative' }}>
+      {isLoading && <Loading />} {/* แสดงหน้าต่าง Loading ถ้า isLoading เป็น true */}
       <Slider {...sliderSettings} ref={(slider) => setSliderRef(slider)}>
         {sliderSchoolData?.map((sliderSchool) => (
           <div key={sliderSchool.id}>
             <img
-              className=' w-full h-[150px] md:h-[500px] object-fill'  
+              className=' w-full h-[150px] md:h-[500px] object-fill'
               src={`https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/${sliderSchool.img1}/public`}
               alt="indexActivity image"
             />
@@ -67,9 +74,8 @@ const IndexPage: React.FC = () => {
           <button
             key={index}
             type='button'
-            className={`w-3 h-3 rounded-full ${
-              activeSlide === index ? 'bg-blue-500' : 'bg-gray-300'
-            }`}
+            className={`w-3 h-3 rounded-full ${activeSlide === index ? 'bg-blue-500' : 'bg-gray-300'
+              }`}
             aria-current={activeSlide === index ? 'true' : 'false'}
             aria-label={`Slide ${index + 1}`}
             data-carousel-slide-to={index}
@@ -78,50 +84,50 @@ const IndexPage: React.FC = () => {
         ))}
       </div>
       <div className='flex justify-between mt-4' style={{ position: 'absolute', width: '100%', top: '50%', transform: 'translateY(-50%)', zIndex: 1 }}>
-  
-   
-   
-  <button onClick={handlePrevSlide} className='text-black mr-2'>
-  <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-        <svg
-          className="w-4 h-4 text-white dark:text-gray-800"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 6 10"
-        >
-          <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M5 1 1 5l4 4"
-          />
-        </svg>
-        <span className="sr-only">Previous</span>
-      </span>
-  </button>
-  <button onClick={handleNextSlide}>
-  <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-        <svg
-          className="w-4 h-4 text-white dark:text-gray-800"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 6 10"
-        >
-          <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="m1 9 4-4-4-4"
-          />
-        </svg>
-        <span className="sr-only">Next</span>
-      </span>
-  </button>
-</div>
+
+
+
+        <button onClick={handlePrevSlide} className='text-black mr-2'>
+          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg
+              className="w-4 h-4 text-white dark:text-gray-800"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 6 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 1 1 5l4 4"
+              />
+            </svg>
+            <span className="sr-only">Previous</span>
+          </span>
+        </button>
+        <button onClick={handleNextSlide}>
+          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg
+              className="w-4 h-4 text-white dark:text-gray-800"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 6 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m1 9 4-4-4-4"
+              />
+            </svg>
+            <span className="sr-only">Next</span>
+          </span>
+        </button>
+      </div>
     </div>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import '@/scss/globals.css'
 import type { AppProps } from 'next/app'
+import Loading from '@/components/loading';
+import React, { useState } from 'react';
+
 
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [isLoading, setIsLoading] = useState(false);
+
+  return (
+    <>
+      {isLoading && <Loading />} {/* แสดงหน้าต่าง Loading ถ้า isLoading เป็น true */}
+      <Component {...pageProps} />
+    </>
+  );
 }
