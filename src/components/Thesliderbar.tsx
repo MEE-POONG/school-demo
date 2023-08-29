25abc2d056e43232db236b83c96e16ab6299a046
ken1lnw <moowinza@gmail.com>
2023-08-29 11:45:24 +0700

    path

--- src/components/Thesliderbar.tsx
+++ src/components/Thesliderbar.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import Loading from './loading';
+import Loading from '@/components/loading';
 
 interface sliderSchool {
   id: number;
