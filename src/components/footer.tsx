172aeace357682d87ee85f43d1806bb1aa8b685a
Ploy <117616170+yaminP@users.noreply.github.com>
2023-09-07 15:46:49 +0700

    footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,67 +1,79 @@
 import Link from "next/link";
 import React from "react";
 
-import LogoPanomwan2 from "@/container/LOGO/Logopanomwan2";
 
 const Footer = () => {
   return (
     <footer className=" bg-[#1F306A] w-full font-fontTH">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex flex-col md:flex-row items-center justify-between">
-          <LogoPanomwan2 />
-
-          <div className="md:flex  text-gray-500 my-8">
-            <div className="md:mr-10">
-              <h2 className="mb-3 text-md font-semibold text-white">
-                คณะที่เปิดสอน
-              </h2>
-              <ul className="text-gray-500">
-                <li className="mb-4">
-                  <p className=" text-white">
-                    คณะบริหารธรุกิจ
-                  </p>
-                </li>
-                <li className="mb-4">
-                  <p className=" text-white">
-                    คณะวิทยาศาสตร์และเทคโนโลยี
-                  </p>
-                </li>
-                <li className="mb-4">
-                  <p className=" text-white">
-                    คณะวิทยาศาสตร์
-                  </p>
-                </li>
-                <li>
-                  <p className=" text-white">
-                    คณะศิลปศาสตร์
-                  </p>
-                </li>
-              </ul>
-            </div>
-
-            <div className="">
-              <h2 className=" mt-3 md:mt-0 py-3 text-md font-semibold text-white">รายการ</h2>
-              <ul className="text-white">
-                <li className="mb-4">
-                  <Link href="/register" className="hover:underline ">
-                    สมัครเรียน
-                  </Link>
-                </li>
-                <li className="mb-4">
-                  <Link href="/advisor" className="hover:underline ">
-                    ปรึกษาเรื่องสมัครเรียน
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
+      <div className="container mx-auto p-5 lg:flex lg:justify-between items-start md:py-8 lg:my-16">
+        <div>
+          <Link href="./" className="flex">
+            <img
+              src="/img/Logo_panomwan.png"
+              className="h-[50px]  md:h-[120px] mr-3"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center text-[10px] md:text-base font-medium text-white">
+              <p>PHANOMWAN COLLEGE OF TECHNOLOGY</p>
+              <hr className="w-full h-[2px] bg-white my-0.5" />
+              <p>198 ถ.มิตรภาพ-จอหอ ต.บ้านโพธิ์ อ.เมือง จ.นครราชสีมา 30310</p>
+              <p>TEL : 044-955121-122 &nbsp;FAX : 044-955120</p>
+            </span>
+          </Link>
         </div>
+
+        <div className="mt-5 md:flex justify-around gap-16">
+          <div className="">
+            <h4 className="md:mb-3 text-[12px] md:text-[18px] md:font-semibold text-white">
+              คณะที่เปิดสอน
+            </h4>
+            <div className="bg-white w-full h-[0.25px] md:hidden mb-1"></div>
+            <ul className="text-white text-[10px] md:text-[15px]">
+              <li className="mb-1">
+                คณะบริหารธรุกิจ
+              </li>
+              <li className="mb-1">
+                คณะวิทยาศาสตร์และเทคโนโลยี
+              </li>
+              <li className="mb-1">
+                คณะวิทยาศาสตร์
+              </li>
+              <li>
+                คณะศิลปศาสตร์
+              </li>
+            </ul>
+          </div>
+
+          <div className="mt-5 md:mt-0">
+            <h4 className=" md:mb-3 text-[12px] md:text-[18px] md:font-semibold text-white">รายการ</h4>
+            <div className="bg-white w-full h-[0.25px] md:hidden mb-1"></div>
+            <ul className="text-white text-[10px] md:text-[15px]">
+              <li className="mb-1">
+                <Link href="/register" className="hover:underline ">
+                  สมัครเรียน
+                </Link>
+              </li>
+              <li className="mb-4">
+                <Link href="/advisor" className="hover:underline ">
+                  ปรึกษาเรื่องสมัครเรียน
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+        </div>
+
       </div>
 
-      <div className=" bg-black w-full md:p-4 p-2">
-        <p className=" text-white font-serif text-center text-xs">
-          © 2023 Me Prompt Technology CONPANY LIMITED. All Rights Reserved.
-        </p>
+
+      <div className=" bg-black w-full p-2">
+        <div className=" text-white text-center text-[9px] md:text-xs">
+          © 2023&nbsp;
+          <Link href="" className="hover:underline hover:text-amber-400">
+            Me Prompt Technology COMPANY LIMITED.
+          </Link>
+          &nbsp;All Rights Reserved.
+        </div>
       </div>
     </footer>
   );
