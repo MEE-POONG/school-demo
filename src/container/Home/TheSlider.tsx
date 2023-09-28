957950c38f1e6b709c9b540700b98389fe7cb37d
Ploy <117616170+yaminP@users.noreply.github.com>
2023-09-28 14:29:39 +0700

    header

--- src/container/Home/TheSlider.tsx
+++ src/container/Home/TheSlider.tsx
@@ -1,4 +1,3 @@
-import { Typography } from '@material-tailwind/react';
 import React, { useEffect, useState } from 'react';
 import Slider from 'react-slick';
 
@@ -47,36 +46,91 @@ const Thesliderbar = () => {
         infinite: true,
         speed: 500,
         slidesToShow: 1,
-        autoplaySpeed: 2000,
-        vertical: true,
-        verticalSwiping: true,
         slidesToScroll: 1,
         beforeChange: (_current: any, next: React.SetStateAction<number>) => setActiveSlide(next),
     };
 
     return (
-        <div className="relative flex h-screen content-center items-center justify-center pt-16 pb-32">
-            <div className="absolute top-0 h-full w-full bg-[url('/img/school/panomtext.png')] bg-cover bg-center" />
-            <div className="absolute top-0 h-full w-full bg-black/75 bg-cover bg-center" />
-            <div className="max-w-8xl container relative mx-auto">
-                <div className="flex flex-wrap items-center">
-                    <div className="ml-auto mr-auto w-full px-4 text-center lg:w-8/12">
-                        <Typography
-                            variant="h3"
-                            color="white"
-                            className="mb-6 font-black"
-                        >
-                            Your story starts with us.
-                        </Typography>
-                        <Typography variant="lead" color="white" className="opacity-80">
-                            This is a simple example of a Landing Page you can build using
-                            Material Tailwind. It features multiple components based on the
-                            Tailwind CSS and Material Design by Google.
-                        </Typography>
+        <>
+            <div className="md:w-full h-2 bg-gradient-to-r from-blue-900 via-yellow-500 to-blue-900"></div>
+            <div id="default-carousel" className="relative bg-gradient-to-r from-blue-900 via-yellow-500 to-blue-900 " data-carousel="static">
+                <div className="slider-container">
+                    {/* <Slider {...sliderSettings} ref={(slider) => setSliderRef(slider)}>
+                        {sliderSchoolData?.map((sliderSchool) => (
+                            <div key={sliderSchool.id}>
+                                <img
+                                    src={`https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/${sliderSchool.img1}/public`}
+                                    className="block top-1/2 left-1/2 w-full"
+                                    alt="indexActivity image"
+                                />
+                            </div>
+                        ))}
+                    </Slider> */}
+                    <div
+                        className='absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2'
+                        style={{ transform: 'translateX(-50%)' }}
+                    >
+                        {sliderSchoolData?.map((_, index) => (
+                            <button
+                                key={index}
+                                type='button'
+                                className={`w-3 h-3 rounded-full ${activeSlide === index ? 'bg-blue-500' : 'bg-gray-300'
+                                    }`}
+                                aria-current={activeSlide === index ? 'true' : 'false'}
+                                aria-label={`Slide ${index + 1}`}
+                                data-carousel-slide-to={index}
+                                onClick={() => handleSlideTo(index)}
+                            ></button>
+                        ))}
+                    </div>
+
+                    <div className='flex justify-between ' style={{ position: 'absolute', width: '100%', top: '50%', transform: 'translateY(-50%)', zIndex: 1 }}>
+                        <button onClick={handlePrevSlide} className='text-black mr-2'>
+                            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                                <svg
+                                    className="w-4 h-4 text-white dark:text-gray-800"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 6 10"
+                                >
+                                    <path
+                                        stroke="currentColor"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M5 1 1 5l4 4"
+                                    />
+                                </svg>
+                                <span className="sr-only">Previous</span>
+                            </span>
+                        </button>
+                        <button onClick={handleNextSlide}>
+                            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                                <svg
+                                    className="w-4 h-4 text-white dark:text-gray-800"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 6 10"
+                                >
+                                    <path
+                                        stroke="currentColor"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="m1 9 4-4-4-4"
+                                    />
+                                </svg>
+                                <span className="sr-only">Next</span>
+                            </span>
+                        </button>
+
                     </div>
                 </div>
             </div>
-        </div>
+
+        </>
     );
 };
 
