0a8c5f0892628f0c42c88d918ec1050c50b99d3e
devilzeros <devilzeros@outlook.co.th>
2023-10-03 23:43:59 +0700

    est

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,8 @@
 import TheLayout from "@/components/TheLayout";
 import { NewNews } from "@/container/Home/News";
-import { CarouselSlider } from "@/container/Home/Carousel";
 import HeadHome from '@/container/Home/headder';
 import Menu from "@/container/Home/menu";
-import SlidNews from "@/container/Home/sliderNew";
 import Statistics from "@/container/Home/statistics";
-import Thesliderbar from "@/container/Home/TheSlider";
 
 
 export default function Index() {
@@ -13,13 +10,9 @@ export default function Index() {
     <>
       <TheLayout>
         <HeadHome />
-        {/* <SlidNews/> */}
         <Menu />
         <NewNews />
         <Statistics />
-        {/* <div className="min-h-screen">
-
-        </div> */}
       </TheLayout>
     </>
   )
