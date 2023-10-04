c6971466e1fc059fb3819b12edbd40302da116fb
Ploy <117616170+yaminP@users.noreply.github.com>
2023-10-04 17:32:41 +0700

    advisor

---
+++ src/pages/advisor/address.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { FaLocationDot } from "react-icons/fa6";
+import { BiLogoFacebookCircle } from "react-icons/bi";
+
+const AddressContact = () => {
+    return (
+        <>
+            <div className="container mx-auto px-2 md:px-14 py-5">
+                <p className="text-light-blue-600">Contact</p>
+                <p className='text-xl md:text-3xl text-[#1F306A]'>
+                    ช่องทางการติดต่อ
+                </p>
+
+                <div className="mt-8">
+                    <div className="flex flex-wrap justify-around">
+                        <div className="text-sm md:text-base">
+                            <div>
+                                <p className=" text-amber-600 font-semibold text-xl mb-2">วิทยาลัยเทคโนโลยีพนมวันท์</p>
+                                <p>ที่อยู่: &nbsp;
+                                    <span>198 ถ.มิตรภาพ-จอหอ ต.บ้านโพธิ์ อ.เมือง จ.นครราชสีมา 30310</span>
+                                </p>
+                                <p>โทร: &nbsp;
+                                    <span>044-955121-122</span>
+                                </p>
+                                <p>แฟ็กซ์: &nbsp;
+                                    <span>044-955121-122</span>
+                                </p>
+                                <p>อีเมล: &nbsp;
+                                    <span>phanomwanu@gmail.com</span>
+                                </p>
+                            </div>
+                            <div className="mt-5">
+                                <p className=" text-amber-600 font-semibold text-xl mb-2 flex">
+                                    พรพิมล ลักขษร
+                                    <span className="text-black font-thin text-lg ml-5">ผู้ช่วยที่ปรึกษา</span>
+                                </p>
+                                <p>โทร: &nbsp;
+                                    <span>061-019-8815</span>
+                                </p>
+                                <p className="flex items-center"><BiLogoFacebookCircle/> 
+                                    <span>Facebook: &nbsp;061-019-8815</span>
+                                </p>
+                            </div>
+                        </div>
+                        {/* <div>
+                            <img className="w-16 h-16 md:w-24 md:h-24 lg:w-32 lg:h-32" src="/img/Logo_panomwan.png" />
+                        </div> */}
+                        <div className="mt-5 md:mt-0">
+                            <iframe
+                                className="md:w-[400px] h-full rounded-lg"
+                                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15410.825965346847!2d102.1841477!3d15.0643421!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x311949e7e29a02b3%3A0x8b0fbbc3758d561f!2z4Lin4Li04LiX4Lii4Liy4Lil4Lix4Lii4LmA4LiX4LiE4LmC4LiZ4LmC4Lil4Lii4Li14Lie4LiZ4Lih4Lin4Lix4LiZ4LiX4LmMIFBoYW5vbXdhbiBDb2xsZWdlIG9mIFRlY2hub2xvZ3k!5e0!3m2!1sth!2sth!4v1689326551888!5m2!1sth!2sth" >
+
+                            </iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+export default AddressContact;
--- src/pages/advisor/index.tsx
+++ src/pages/advisor/index.tsx
@@ -1,15 +1,9 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import TheLayout from '@/components/TheLayout'
-import { BsBuildingFill } from "react-icons/bs";
-import { IoNewspaperSharp } from "react-icons/io5";
-import { PiHandCoinsBold } from "react-icons/pi";
-import { GoBook } from "react-icons/go";
-import { MdSupportAgent } from "react-icons/md";
-import { TbStars } from "react-icons/tb";
-import { FaLocationDot } from "react-icons/fa6";
 import { useState, useEffect } from 'react'; // เพิ่มการ import useEffect
 import Loading from '@/components/loading'; // เพิ่มการ import คอมโพเนนต์ Loading
+import AddressContact from './address';
 
 
 
@@ -63,7 +57,6 @@ export default function Welfare() {
                 {/* ขีด */}
                 <div className='bg-gradient-to-r from-[#1F306A] via-[#FFBF00] to-black w-full h-1 '></div>
             </div>
-
 
             {/* เนื้อหาส่วนทีมที่ปรึกษา */}
             <div className='bg-[#1F306A]/80  bg-opacity-80 px-2 md:px-14 pb-10'>
@@ -205,148 +198,12 @@ export default function Welfare() {
                 {/* ปิด เนื้อหา */}
             </div>
 
-
-            {/* ช่องทางการติดต่อ กับ ขีด */}
-            <div className=' px-2 md:px-14 py-5'>
-                <p className='text-2xl  md:text-5xl text-[#1F306A]'>
-                    ช่องทางการติดต่อ
-                </p>
-                {/* ขีด */}
-                <div className='bg-gradient-to-r from-[#1F306A] via-[#FFBF00] to-black w-full h-1 '></div>
+            <div>
+                <AddressContact/>
             </div>
 
-            {/* เนื้อหาส่วนช่องทางการติดต่อ */}
-            <div className='bg-black bg-opacity-80 px-2 md:px-14 pb-10'>
-                <div className='py-4'>
-                    <p className='text-[#FFBF00] text-lg mx-a'>วิทยาลัยเทคโนโลยีพนมวันท์</p>
-                </div>
-                {/* ที่อยู่วิทยาลัย */}
-                {/* <div className='grid grid-cols-12 text-white mb-5 lg:text-lg md:text-sm text-[7px]  '>
-                    <div className='col-span-1 space-y-2'>
-                        <p>ที่อยู่</p>
-                        <p>โทร</p>
-                        <p>แฟ็กซ์</p>
-                        <p>อีเมล</p>
-                    </div>
 
-                    <div className='col-span-7 space-y-2' >
-                        <p>198 ถ.มิตรภาพ-จอหอ ต.บ้านโพธิ์ อ.เมือง จ.นครราชสีมา 30310</p>
-                        <p>044-955121-122</p>
-                        <p>044-955-120</p>
-                        <p>phanomwanu@gmail.com</p>
-                    </div>
-
-                    <div className='col-span-4 flex justify-center' >
-                        <img className="w-12 h-12 md:w-24 md:h-24 lg:w-32 lg:h-32" src="/img/Logo_panomwan.png" />
-                    </div>
-
-                </div> */}
-
-
-                <div className='grid grid-rows-12 grid-flow-col gap-1 grid-cols-12 text-white mb-5 lg:text-lg text-sm md:text-md   '>
-
-                    <div className='row-start-1 col-start-1'>
-                        <p>ที่อยู่</p>
-                    </div>
-                    <div className='row-start-1 col-start-3 md:col-start-2 col-span-7'>
-                        <p>198 ถ.มิตรภาพ-จอหอ ต.บ้านโพธิ์ อ.เมือง จ.นครราชสีมา 30310</p>
-                    </div>
-
-                    <div className='row-start-3 col-start-1'>
-                        <p>โทร</p>
-                    </div>
-                    <div className='row-start-3 col-start-3 md:col-start-2 col-span-7'>
-                        <p>044-955121-122</p>
-                    </div>
-
-                    <div className='row-start-5 col-start-1'>
-                        <p>แฟ็กซ์</p>
-                    </div>
-                    <div className='row-start-5 col-start-3 md:col-start-2 col-span-7'>
-                        <p>044-955121-122</p>
-                    </div>
-
-
-                    <div className='row-start-7 col-start-1'>
-                        <p>อีเมล</p>
-                    </div>
-                    <div className='row-start-7 col-start-3 md:col-start-2 col-span-7'>
-                        <p>phanomwanu@gmail.com</p>
-                    </div>
-
-                    <div className='col-start-10 col-span-3 row-start-1 row-end-7' >
-                        <img className="w-16 h-16 md:w-24 md:h-24 lg:w-32 lg:h-32" src="/img/Logo_panomwan.png" />
-                    </div>
-                </div>
-
-
-                {/* ขีดเทา */}
-                <div className='bg-[#536878] w-full h-0.5 '></div>
-
-                <div className='py-4 flex gap-2 '>
-                    <p className='text-[#FFBF00] text-lg'>พรพิมล ลักขษร</p>
-                    <h2 className='text-white text-md my-auto ml-2'>ผู้ช่วยที่ปรึกษา</h2>
-                </div>
-                {/* ที่อยู่อาจารย์ */}
-
-                <div className='grid grid-rows-12 grid-flow-col gap-1 grid-cols-12 text-white mb-5 lg:text-lg text-sm md:text-md   '>
-
-                    <div className='row-start-1 col-start-1'>
-                        <p>ที่อยู่</p>
-                    </div>
-                    <div className='row-start-1 col-start-3 md:col-start-2 col-span-7'>
-                        <p>198 ถ.มิตรภาพ-จอหอ ต.บ้านโพธิ์ อ.เมือง จ.นครราชสีมา 30310</p>
-                    </div>
-
-                    <div className='row-start-3 col-start-1'>
-                        <p>โทร</p>
-                    </div>
-                    <div className='row-start-3 col-start-3 md:col-start-2 col-span-7'>
-                        <p>061-019-8815</p>
-                    </div>
-
-                    <div className='row-start-5 col-start-1'>
-                        <p>Faecbook</p>
-                    </div>
-                    <div className='row-start-5 col-start-3 md:col-start-2 md:pl-6 lg:pl-0 col-span-7'>
-                        <p className='ml-2 md:ml-0'>teacher Pornpimol</p>
-                    </div>
-
-                    <div className='col-start-10 col-span-3 row-start-1 row-end-7' >
-                        <img className="w-16 h-16 md:w-24 md:h-24 lg:w-32 lg:h-32" src="/img/Qrline.jpg" />
-                    </div>
-                </div>
-
-                {/* <div className='grid grid-cols-12 text-white mb-5 lg:text-lg md:text-sm text-[7px] '>
-                    <div className='col-span-1 space-y-2'>
-                        <p>ที่อยู่</p>
-                        <p>โทร</p>
-                        <p>Faecbook</p>
-                    </div>
-
-                    <div className='col-span-7 space-y-2 md:ml-4 lg:ml-0' >
-                        <p>198 ถ.มิตรภาพ-จอหอ ต.บ้านโพธิ์ อ.เมือง จ.นครราชสีมา 30310</p>
-                        <p>061-019-8815</p>
-                        <p className='ml-2 md:ml-0'>teacher Pornpimol</p>
-                    </div>
-
-                    <div className='col-span-4 flex justify-center' >
-                        <img className="w-12 h-12 md:w-24 md:h-24 lg:w-32 lg:h-32" src="/img/Qrline.jpg" />
-                    </div>
-
-                </div> */}
-                {/* map */}
-                <div className='flex justify-center'>
-                    <iframe className="  md:w-full h-72 rounded-lg" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15410.825965346847!2d102.1841477!3d15.0643421!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x311949e7e29a02b3%3A0x8b0fbbc3758d561f!2z4Lin4Li04LiX4Lii4Liy4Lil4Lix4Lii4LmA4LiX4LiE4LmC4LiZ4LmC4Lil4Lii4Li14Lie4LiZ4Lih4Lin4Lix4LiZ4LiX4LmMIFBoYW5vbXdhbiBDb2xsZWdlIG9mIFRlY2hub2xvZ3k!5e0!3m2!1sth!2sth!4v1689326551888!5m2!1sth!2sth" ></iframe>
-                </div>
-
-                <div className='md:px-48'>
-                    <div className='bg-black rounded-lg mt-2  flex justify-center items-center drop-shadow-lg'>
-                        <FaLocationDot className='text-[#FFBF00] mr-2 text-center' />
-                        <p className='text-2xl text-[#FFBF00] text-center'>Main Campus</p>
-                    </div>
-                </div>
-            </div>
+        
 
 
 
