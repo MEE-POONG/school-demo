31ca6ce639b878a3c41a72ccaca00cab127e200d
devilzeros <devilzeros@outlook.co.th>
2023-10-05 17:04:26 +0700

    est

--- src/container/Home/headder.tsx
+++ src/container/Home/headder.tsx
@@ -29,30 +29,6 @@ const HeadHome = () => {
             });
     }, []);
 
-    const handlePrevSlide = () => {
-        sliderRef?.slickPrev();
-    };
-
-    const handleNextSlide = () => {
-        sliderRef?.slickNext();
-    };
-
-    const handleSlideTo = (index: number) => {
-        setActiveSlide(index);
-        sliderRef?.slickGoTo(index);
-    };
-
-    const sliderSettings = {
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        autoplaySpeed: 2000,
-        vertical: true,
-        verticalSwiping: true,
-        slidesToScroll: 1,
-        beforeChange: (_current: any, next: React.SetStateAction<number>) => setActiveSlide(next),
-    };
 
     return (
         <div className="relative flex h-screen content-center items-center justify-center pt-24">
@@ -65,10 +41,10 @@ const HeadHome = () => {
                             <h1 className="mt-1 text-lg xl:text-4xl font-semibold text-white sm:text-slate-900 md:text-2xl dark:sm:text-white">Beach House in Collingwood</h1>
                             <p className="text-sm xl:text-2xl leading-4 font-medium text-white sm:text-slate-500 dark:sm:text-slate-400">Entire house</p>
                         </div>
-                        <div className="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0 animate-fade-left">
-                            <img src={`/img/school/panomtext.png`} alt="" className="w-full h-60 object-cover rounded-lg sm:h-52 sm:col-span-2 lg:col-span-full" loading="lazy" />
-                            <img src={`/img/school/panomtext.png`} alt="" className="hidden w-full h-52 object-cover rounded-lg sm:block sm:col-span-2 md:col-span-1 lg:row-start-2 lg:col-span-2 lg:h-64" loading="lazy" />
-                            <img src={`/img/school/panomtext.png`} alt="" className="hidden w-full h-52 object-cover rounded-lg md:block lg:row-start-2 lg:col-span-2 lg:h-64" loading="lazy" />
+                        <div className="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0">
+                            <img src={`https://imagedelivery.net/${""}/600`} alt="" className="w-full h-60 object-cover rounded-lg sm:h-52 sm:col-span-2 lg:col-span-full" loading="lazy" />
+                            <img src={`https://imagedelivery.net/${""}/600`}alt="" className="hidden w-full h-52 object-cover rounded-lg sm:block sm:col-span-2 md:col-span-1 lg:row-start-2 lg:col-span-2 lg:h-64" loading="lazy" />
+                            <img src={`https://imagedelivery.net/${""}/600`} alt="" className="hidden w-full h-52 object-cover rounded-lg md:block lg:row-start-2 lg:col-span-2 lg:h-64" loading="lazy" />
                         </div>
                         <dl className="mt-4 text-xs font-medium flex items-center row-start-2 sm:mt-1 sm:row-start-3 md:mt-2.5 lg:row-start-2 animate-fade-right">
                             <dt className="sr-only">Reviews</dt>
