182d0c781985cb8d81890ab10b202319b9c2b235
devilzeros <devilzeros@outlook.co.th>
2023-10-18 13:37:04 +0700

    est

--- src/container/Sreact.tsx
+++ src/container/Sreact.tsx
@@ -1,9 +1,7 @@
 import Aos from 'aos';
 import React, { useEffect } from 'react';
-import SwiperCore, { Navigation, Autoplay } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-SwiperCore.use([Navigation, Autoplay]);
+import { Navigation } from 'swiper/modules';
 
 const SwiperComponent: React.FC = () => {
 
@@ -17,17 +15,12 @@ const SwiperComponent: React.FC = () => {
     <div className='container m-auto mt-24'>
       <div className='shadow-lg' data-aos="fade-down">
         <Swiper
-          spaceBetween={30}
-          centeredSlides={true}
+          navigation={true} modules={[Navigation]}
+          loop={true}
           autoplay={{
             delay: 2500,
             disableOnInteraction: true
           }}
-          pagination={{
-            clickable: true
-          }}
-          navigation={true}
-          loop={true}  // Add this line for infinite loop
         >
           <SwiperSlide>
             <img className="shadow-xl" src="/img/register/b1.png" alt="Slide Image" />
