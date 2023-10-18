d9cdad1c6404f239a23d1dcaa0040d2943ed0abb
devilzeros <devilzeros@outlook.co.th>
2023-10-18 12:45:54 +0700

    est

--- src/container/Sreact.tsx
+++ src/container/Sreact.tsx
@@ -1,32 +1,42 @@
-import React from 'react';
+import Aos from 'aos';
+import React, { useEffect } from 'react';
 import SwiperCore, { Navigation, Autoplay } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 SwiperCore.use([Navigation, Autoplay]);
 
 const SwiperComponent: React.FC = () => {
+
+  useEffect(() => {
+    Aos.init({
+      duration: 1000,
+    });
+  }, []);
+
   return (
-    <div className='shadow-lg'>
-      <Swiper
-         spaceBetween={30}
-         centeredSlides={true}
-         autoplay={{
-           delay: 2500,
-           disableOnInteraction: true
-         }}
-         pagination={{
-           clickable: true
-         }}
-         navigation={true}
-         loop={true}  // Add this line for infinite loop
-      >
-        <SwiperSlide>
-          <img className="shadow-xl" src="/img/register/b1.png" alt="Slide Image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className="shadow-xl" src="/img/register/b2.png" alt="Slide Image" />
-        </SwiperSlide>
-      </Swiper>
+    <div className='container m-auto mt-24'>
+      <div className='shadow-lg' data-aos="fade-down">
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: true
+          }}
+          pagination={{
+            clickable: true
+          }}
+          navigation={true}
+          loop={true}  // Add this line for infinite loop
+        >
+          <SwiperSlide>
+            <img className="shadow-xl" src="/img/register/b1.png" alt="Slide Image" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="shadow-xl" src="/img/register/b2.png" alt="Slide Image" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
 }
