7216c000a0d919ebe840eb09f2cb3730d3ddccdf
devilzeros <devilzeros@outlook.co.th>
2023-10-26 17:41:45 +0700

    est

--- src/data/news.ts
+++ src/data/news.ts
@@ -1,13 +1,8 @@
 export const newsMenu = [
     {
-        label: "ทั้งหมด",
-        value: "",
-    },
-    {
         label: "ประชาสัมพันธ์",
         value: "News",
     },
-
     {
         label: "กิจกรรม",
         value: "Activity",
