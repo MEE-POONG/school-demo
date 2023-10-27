f807ce9f101f88273d1134074b13c9cfb3fe3507
devilzeros <devilzeros@outlook.co.th>
2023-10-27 15:55:52 +0700

    est

--- src/container/Home/NewNews.tsx
+++ src/container/Home/NewNews.tsx
@@ -107,78 +107,7 @@ export const NewNews: React.FC = () => {
 
   return (
     <div className="container m-auto">
-      {isLoading && <Loading />}
-      <TitleText titleText={"ข่าว & กิจกรรม"} titleTextTo={"“พนมวันท์”"} />
-      <Tabs id="custom-animation" value={selectType} data-aos="fade-right">
-        <TabsHeader className="bg-yellow-800 "
-          indicatorProps={{
-            className: "bg-yellow-800 shadow-none !text-gray-900",
-          }}
-        >
-          {newsMenu.map(({ label, value }) => (
-            <Tab
-              key={value}
-              value={value}
-              className={`font-bold text-white `}
-              onClick={() => setSelectType(value)}
-            >
-              {label}
-            </Tab>
-          ))}
-        </TabsHeader>
-        <TabsBody
-          animate={{
-            initial: { y: 250 },
-            mount: { y: 0 },
-            unmount: { y: 250 },
-          }}
-        >
-          <Slider {...settings}>
-          {newsArray?.map((news) => (
-              <div key={news?.id}>
-                <Card className="my-6 w-72 overflow-hidden mx-auto">
-                  <CardHeader floated={false} shadow={false} color="transparent" className="m-0 rounded-none">
-                    <img className="h-48 object-cover"
-                      src={`https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/${news?.img || "4500f404-dbac-40f3-6696-ae768a38e800"}/700`}
-                      alt={news?.title || "Image Alt Text"}
-                    />
-                  </CardHeader>
-                  <CardBody>
-                    <Typography variant="h6" color="blue-gray" className="title-clamp">
-                      {news?.title}
-                    </Typography>
-                    <Typography variant="lead" color="gray" className="mt-3 font-normal text-sm subtitle-clamp">
-                      {news?.subTitle}
-                    </Typography>
-                  </CardBody>
-                  <CardFooter className="pt-0">
-                    <Link href={`/ReadNews?id=${news?.id}`} className="flex w-fit mx-auto items-center bg-blue-400 text-white hover:bg-yellow-800 px-6 py-2 rounded-lg">
-                      รายละเอียด
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                        className="h-4 w-4"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-                        />
-                      </svg>
-                    </Link>
-                  </CardFooter>
-                </Card>
-              </div>
-            ))}
-          </Slider>
-        </TabsBody>
-      </Tabs>
-      <button type="button" className="text-yellow-800 hover:text-yellow-900  text-sm leading-6 font-medium py-2 px-3 rounded-lg ">
-        <Link href="./ShowContentAll">ดูข่าวทั้งหมด {">>>>"}</Link>
-      </button>
+      sss
     </div>
   );
 }
