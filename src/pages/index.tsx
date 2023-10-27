1fe03ef065670e253163dd5de6ee825537807795
devilzeros <devilzeros@outlook.co.th>
2023-10-27 15:48:27 +0700

    est

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import TheLayout from "@/components/TheLayout";
 import { ContactUS } from "@/container/ContactUS";
 import { NewNews } from "@/container/Home/NewNews";
+import { Relations } from "@/container/Home/Relations";
 import HeadHome from '@/container/Home/headder';
 import { Menu } from "@/container/Home/menu";
 import Statistics from "@/container/Home/statistics";
-import { Relations } from "@/container/Home/Relations";
 import HomeSlider from '@/container/Home/homeslide';
 
 export default function Index() {
