f6d9de6d8cc30e807a15fc5e1b35a7b26049ce89
Ploy <117616170+yaminP@users.noreply.github.com>
2023-10-27 18:19:22 +0700

    readnews

--- src/pages/news/[id].tsx
+++ src/pages/news/[id].tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/router";
 import Image from 'next/image';
 import { AiTwotoneCalendar } from "react-icons/ai";
 import { FaHandPointRight } from "react-icons/fa";
+import TheLayout from "@/components/TheLayout";
 
 
 const ReadNews: React.FC = () => {
@@ -48,36 +49,37 @@ const ReadNews: React.FC = () => {
   }
 
   return (
-    <div className="container mx-auto py-16">
-      <div className="mt-24 px-5 lg:px-0">
-        <h3 className="text-xl md:text-3xl font-semibold">{news.title}</h3>
+    <TheLayout>
+      <div className="container mx-auto py-16">
+        <div className="mt-24 px-5 lg:px-0">
+          <h3 className="text-xl md:text-3xl font-semibold">{news.title}</h3>
 
-        <div className="h-0.5 bg-blue-400 w-full my-2 rounded-full"></div>
-        <img
-          className="w-full h-auto lg:h-[450px] object-cover rounded-md shadow-lg"
-          src={`https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/${
-            news.promoteImg
-          }/700`}
-          width={1024}
-          height={100}
-          alt="Pictue of News"
-        />
-        <p className="flex items-center mt-10 text-xs md:text-base">
-          <AiTwotoneCalendar /> &nbsp;
-          วันที่ :<span className="text-blue-400">&nbsp;5 ตุลาคม 2566</span>
-        </p>
-        <div className="mt-3 bg-white p-7 shadow-lg rounded-md">
-          <div className="text-xs md:text-base indent-8 ">
-            {news.detail}
+          <div className="h-0.5 bg-blue-400 w-full my-2 rounded-full"></div>
+          <img
+            className="w-full h-auto lg:h-[450px] object-cover rounded-md shadow-lg"
+            src={`https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/${news.promoteImg
+              }/700`}
+            width={1024}
+            height={100}
+            alt="Pictue of News"
+          />
+          <p className="flex items-center mt-10 text-xs md:text-base">
+            <AiTwotoneCalendar /> &nbsp;
+            วันที่ :<span className="text-blue-400">&nbsp;5 ตุลาคม 2566</span>
+          </p>
+          <div className="mt-3 bg-white p-7 shadow-lg rounded-md">
+            <div className="text-xs md:text-base indent-8 ">
+              {news.detail}
+            </div>
+            <a href="#" className="text-xs md:text-base flex items-center mt-8">
+              <FaHandPointRight className="animate-pulse animate-infinite animate-duration-[900ms] text-amber-800" />
+              <span className="ml-2 text-blue-400 hover:underline">ต้องการโหลดรูปภาพ คลิก</span>
+            </a>
           </div>
-          <a href="#" className="text-xs md:text-base flex items-center mt-8">
-            <FaHandPointRight className="animate-pulse animate-infinite animate-duration-[900ms] text-amber-800" />
-            <span className="ml-2 text-blue-400 hover:underline">ต้องการโหลดรูปภาพ คลิก</span>
-          </a>
-        </div>
 
+        </div>
       </div>
-    </div>
+    </TheLayout>
   );
 };
 
