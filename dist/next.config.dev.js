b5d62ce9841d58b5c2fddf693afe235eb8bef164
devilzeros <devilzeros@outlook.co.th>
2023-10-27 18:59:05 +0700

    est

--- dist/next.config.dev.js
+++ dist/next.config.dev.js
@@ -1,12 +1,14 @@
+"use strict";
+
 module.exports = {
-    experimental: {
-        nextScriptWorkers: true,
-    },
+  experimental: {
+    nextScriptWorkers: true
+  },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
     // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-}
+    ignoreBuildErrors: true
+  }
+};
