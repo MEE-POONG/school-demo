315038e37dd8a2e26e931ea2d62df7af22dfa3cd
devilzeros <devilzeros@outlook.co.th>
2023-11-02 02:34:42 +0700

    est

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import TheLayout from "@/components/TheLayout";
 import { ContactUS } from "@/container/ContactUS";
+
 import { NewNews } from "@/container/Home/NewNews";
 import { Relations } from "@/container/Home/Relations";
 import HeadHome from '@/container/Home/headder';
 import { Menu } from "@/container/Home/menu";
 import Statistics from "@/container/Home/statistics";
-import { HomeSlider } from '@/container/Home/HomeSlide';
 
 export default function Index() {
   return (
     <TheLayout>
       <HeadHome />
       <Menu />
-      <HomeSlider/>
+   
       <NewNews />
       <Relations />
       <Statistics />
