6375383b567c1b69c9e659d57ba189fbbb21d66c
Ploy <117616170+yaminP@users.noreply.github.com>
2023-11-09 18:25:03 +0700

    apiheadpage

---
+++ src/pages/api/headpage/index.tsx
@@ -0,0 +1,22 @@
+// pages/api/users.ts
+
+import { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ message: 'Method Not Allowed' });
+  }
+
+  try {
+    const DataHeadpage = await prisma.headPage.findMany();
+    res.status(200).json(DataHeadpage);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
