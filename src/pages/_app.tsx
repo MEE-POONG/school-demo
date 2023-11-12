ab0b64452a0d0870fecb4dfc7f59cef554d692bc
devilzeros <devilzeros@outlook.co.th>
2023-11-13 03:20:55 +0700

    est

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,32 +1,36 @@
 // pages/_app.js
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Loading from "@/components/loading";
 import "@/scss/globals.scss";
-import Head from 'next/head';
 import type { AppProps } from "next/app";
 import AppContextData from "@/context";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const [isLoading, setIsLoading] = useState(false);
-
-  // Define your context value
-  const contextValue = {
+  const [contextValue, setContextValue] = useState({
     courseGroupsCount: 0,
     courseListCount: 0,
     studyingCounts: 0,
     ipAddressesCount: 0,
     contactData: [],
-    // Add more fields or functions as needed
-  };
+  });
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch('/api/IPAddress')
+      .then(response => response.json())
+      .then(data => {
+        setContextValue(prev => ({
+          ...prev,
+          ipAddressesCount: data?.length,
+        }));
+        setIsLoading(false);
+      });
+  }, []);
 
   return (
     <AppContextData.Provider value={contextValue}>
-      <Head>
-        <link 
-          href="https://fonts.googleapis.com/css2?family=Kanit:wght@400&display=swap" 
-          rel="stylesheet" 
-        />
-      </Head>
+      
       {isLoading && <Loading />}
       <main>
         <Component {...pageProps} />
